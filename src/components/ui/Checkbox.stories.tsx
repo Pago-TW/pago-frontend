bff594bf3b61c1c3215420153ef84058597047cf
Kerati <enderwolf50@gmail.com>
2022-12-14 03:18:19 +0800

    chore(storybook): add figma view

--- src/components/ui/Checkbox.stories.tsx
+++ src/components/ui/Checkbox.stories.tsx
@@ -4,6 +4,12 @@ import { Checkbox } from "./Checkbox";
 export default {
   title: "UI/Checkbox",
   component: Checkbox,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/dinIbshcxJCt2c1jtN1Gm6/Pago---UI-Kit?node-id=48%3A126&t=oa1ENLEM9s30o6TL-1",
+    },
+  },
   argTypes: {
     size: {
       control: {
--- src/components/ui/Radio.stories.tsx
+++ src/components/ui/Radio.stories.tsx
@@ -4,6 +4,12 @@ import { Radio } from "./Radio";
 export default {
   title: "UI/Radio",
   component: Radio,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/dinIbshcxJCt2c1jtN1Gm6/Pago---UI-Kit?node-id=48%3A126&t=oa1ENLEM9s30o6TL-1",
+    },
+  },
   argTypes: {
     size: {
       control: {
