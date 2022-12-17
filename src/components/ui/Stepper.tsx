be65281780713768cb3426918061e70a4898413c
Kerati <enderwolf50@gmail.com>
2022-12-17 20:21:33 +0800

    fix(stepper): hide the connector when the viewport is smaller than the `sm` breakpoint

--- src/components/ui/Stepper.tsx
+++ src/components/ui/Stepper.tsx
@@ -1,9 +1,21 @@
 import type { StepperProps as MuiStepperProps } from "@mui/material";
+import {
+  stepConnectorClasses,
+  Stepper as MuiStepper,
+  styled,
+} from "@mui/material";
 import { StepConnector } from "./StepConnector";
 
 type StepperProps = MuiStepperProps;
 
-const StyledStepper = styled(MuiStepper)<StepperProps>();
+const StyledStepper = styled(MuiStepper)<StepperProps>(({ theme }) => ({
+  [`& .${stepConnectorClasses.root}.${stepConnectorClasses.horizontal}`]: {
+    opacity: 0,
+    [theme.breakpoints.up("sm")]: {
+      opacity: 1,
+    },
+  },
+}));
 
 export const Stepper = ({ connector, ...rest }: StepperProps) => {
   return <StyledStepper connector={connector ?? <StepConnector />} {...rest} />;
