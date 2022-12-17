368fb0766492c20b06c34e1135c4d6c2e2f03b68
Kerati <enderwolf50@gmail.com>
2022-12-17 20:19:26 +0800

    feat(stepper): use ui/StepConnector as default connector when no connector value passed in

--- src/components/ui/Stepper.tsx
+++ src/components/ui/Stepper.tsx
@@ -1,8 +1,10 @@
 import type { StepperProps as MuiStepperProps } from "@mui/material";
-import { Stepper as MuiStepper, styled } from "@mui/material";
+import { StepConnector } from "./StepConnector";
 
 type StepperProps = MuiStepperProps;
 
 const StyledStepper = styled(MuiStepper)<StepperProps>();
 
-export const Stepper = StyledStepper;
+export const Stepper = ({ connector, ...rest }: StepperProps) => {
+  return <StyledStepper connector={connector ?? <StepConnector />} {...rest} />;
+};
