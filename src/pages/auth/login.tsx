596c458372de30f0d0fa1c202daf832687695748
Kerati <enderwolf50@gmail.com>
2023-01-16 20:36:40 +0800

    fix(login): remove paper

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -8,7 +8,6 @@ import {
   Box,
   IconButton,
   InputAdornment,
-  Paper,
   Stack,
   TextField,
 } from "@mui/material";
@@ -26,47 +25,40 @@ const LoginPage: NextPage = () => {
   return (
     <>
       <Header />
-      <Typography
-        variant="h1"
-        weightPreset="bold"
-        textAlign="center"
-        marginY={4}
-      >
-        登入 Pago
-      </Typography>
-      <Paper elevation={3} sx={{ m: "auto", p: 2, maxWidth: 350 }}>
-        <Stack sx={{ gap: 3 }}>
-          <Button variant="outlined" startIcon={<FcGoogle />}>
-            使用 Google 繼續
-          </Button>
-          <Divider>或</Divider>
-          <TextField variant="outlined" label="帳號" required />
-          <TextField
-            type={showPassword ? "text" : "password"}
-            variant="outlined"
-            label="密碼"
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={handleShowPassword}>
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-            required
-          />
-          <Button>登入</Button>
-          <Box display="flex" justifyContent="space-between">
-            <Typography variant="h6">
-              <Link>忘記密碼？</Link>
-            </Typography>
-            <Typography variant="h6">
-              還沒有帳號嗎？<Link href="/auth/signup">立即註冊</Link>
-            </Typography>
-          </Box>
-        </Stack>
-      </Paper>
+      <Stack sx={{ maxWidth: 400, mx: "auto", my: "12vh", p: 2, gap: 3 }}>
+        <Typography variant="h1" weightPreset="bold" textAlign="center">
+          登入
+        </Typography>
+        <Button variant="outlined" startIcon={<FcGoogle />}>
+          使用 Google 繼續
+        </Button>
+        <Divider>或</Divider>
+        <TextField variant="outlined" label="帳號" required />
+        <TextField
+          type={showPassword ? "text" : "password"}
+          variant="outlined"
+          label="密碼"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleShowPassword}>
+                  {showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+          required
+        />
+        <Button>登入</Button>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="h6">
+            <Link>忘記密碼？</Link>
+          </Typography>
+          <Typography variant="h6">
+            還沒有帳號嗎？<Link href="/auth/signup">立即註冊</Link>
+          </Typography>
+        </Box>
+      </Stack>
     </>
   );
 };
