a57fdc9e285960e1e9b569b370051393b4508bea
Kerati <enderwolf50@gmail.com>
2023-02-13 19:19:51 +0800

    feat(header): header component prototype

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,37 @@
 import MenuIcon from "@mui/icons-material/Menu";
-import { AppBar, Button, IconButton, styled, Toolbar } from "@mui/material";
+import { AppBar, Button, IconButton, Toolbar } from "@mui/material";
 import { Typography } from "./ui/Typography";
-
-const LoginButton = styled(Button)(({}) => ({
-  width: 128,
-  color: "white",
-  borderColor: "white",
-  "&:hover": {
-    borderColor: "white",
-  },
-}));
 
 export const Header = () => {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h3" weightPreset="bold" sx={{ flexGrow: 1 }}>
-          Pago
-        </Typography>
-        <LoginButton variant="outlined">登入</LoginButton>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h3" weightPreset="bold" sx={{ flexGrow: 1 }}>
+            Pago
+          </Typography>
+          <Button
+            variant="outlined"
+            sx={{
+              width: 128,
+              color: "white",
+              borderColor: "white",
+              "&:hover": { borderColor: "white" },
+            }}
+          >
+            登入
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
