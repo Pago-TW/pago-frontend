d60df4707ddf279945c0413a33a127bd9f69400f
Kerati <enderwolf50@gmail.com>
2023-02-28 01:06:06 +0800

    fix(detail-item): use media query to decide `labelVariant` and `valueVariant` if not provided

--- src/components/DetailItem.tsx
+++ src/components/DetailItem.tsx
@@ -1,3 +1,4 @@
+import { useMediaQuery } from "@hooks/useMediaQuery";
 import { Stack } from "@mui/material";
 import type { ReactNode } from "react";
 import type { TypographyProps } from "./ui/Typography";
@@ -17,25 +18,33 @@ export const DetailItem = ({
   label,
   value,
   valueBold,
-  labelVariant = "h5",
-  valueVariant = "h5",
-}: DetailItemProps) => (
-  <Stack
-    direction={multiLine ? "column" : "row"}
-    spacing={2}
-    justifyContent="space-between"
-    alignItems={multiLine ? "flex-start" : "center"}
-  >
-    {/* 標籤 */}
-    <Typography variant={labelVariant} color="base.400">
-      {label}
-    </Typography>
-    {/* 數值 */}
-    <Typography
-      variant={valueVariant}
-      weightPreset={valueBold ? "bold" : "normal"}
+  labelVariant,
+  valueVariant,
+}: DetailItemProps) => {
+  const mdDown = useMediaQuery((theme) => theme.breakpoints.down("md"));
+
+  return (
+    <Stack
+      direction={multiLine ? "column" : "row"}
+      spacing={2}
+      justifyContent="space-between"
+      alignItems={multiLine ? "flex-start" : "center"}
     >
-      {value}
-    </Typography>
-  </Stack>
-);
+      {/* 標籤 */}
+      <Typography
+        variant={labelVariant ?? (mdDown ? "h5" : "h4")}
+        color="base.400"
+        sx={{ flexShrink: 0 }}
+      >
+        {label}
+      </Typography>
+      {/* 數值 */}
+      <Typography
+        variant={valueVariant ?? (mdDown ? "h5" : "h4")}
+        weightPreset={valueBold ? "bold" : "normal"}
+      >
+        {value}
+      </Typography>
+    </Stack>
+  );
+};
