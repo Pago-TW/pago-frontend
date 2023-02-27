52058cc4c5121b111ecbdfebf5aa7ee615da26f1
Kerati <enderwolf50@gmail.com>
2023-02-28 01:08:54 +0800

    feat(/commissions/{id}): add pc size commission detail page (without frame)

--- src/pages/commissions/[id].tsx
+++ src/pages/commissions/[id].tsx
@@ -4,6 +4,7 @@ import { Container } from "@components/layouts/Container";
 import { PageTitle } from "@components/PageTitle";
 import { Link } from "@components/ui/Link";
 import { Typography } from "@components/ui/Typography";
+import { useMediaQuery } from "@hooks/useMediaQuery";
 import { Place } from "@mui/icons-material";
 import {
   Avatar,
@@ -17,9 +18,43 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
+const BuyingAgentArea = () => {
+  return (
+    <>
+      <Stack spacing={3} sx={{ width: "100%" }}>
+        {/* 代購者 */}
+        <Typography variant="h3" mt={1} sx={{ textAlign: "center" }}>
+          願意代購者
+        </Typography>
+        <BuyingAgent />
+        <BuyingAgent />
+        <Stack alignItems="center">
+          <Link fontSize={18} mt={1}>
+            顯示更多
+          </Link>
+        </Stack>
+      </Stack>
+      <Stack direction="row" spacing={2} sx={{ "&&": { mt: 5 } }}>
+        {/* 更多代購者 */}
+        <AvatarGroup total={100}>
+          <Avatar />
+          <Avatar />
+          <Avatar />
+          <Avatar />
+        </AvatarGroup>
+        <Typography variant="h6" color="base.400">
+          另有100位代購者的旅途與此委託相符
+        </Typography>
+      </Stack>
+    </>
+  );
+};
+
 const CommissionDetailPage: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
+
+  const xs = useMediaQuery((theme) => theme.breakpoints.between("xs", "md"));
 
   return (
     <>
@@ -31,19 +66,49 @@ const CommissionDetailPage: NextPage = () => {
           <PageTitle title="委託詳情" sharable />
         </Box>
         <Stack alignItems="center" mb={4}>
-          <Stack spacing={2} sx={{ width: 336 }}>
-            <Paper elevation={3} sx={{ p: 2 }}>
-              <Stack spacing={2}>
-                {/* 圖片 */}
-                <Skeleton variant="rectangular" height={300} width={300} />
-                {/* 名稱 */}
-                <Typography variant="h3" sx={{ textAlign: "center" }}>
-                  商品名稱
-                </Typography>
-              </Stack>
-            </Paper>
-            <Paper elevation={3} sx={{ p: 2 }}>
+          <Stack
+            direction={xs ? "column" : "row"}
+            spacing={xs ? 2 : 4}
+            sx={{
+              width: "100%",
+              maxWidth: { xs: 336, md: 1440 },
+              px: { md: 4 },
+            }}
+          >
+            <Stack spacing={3} alignItems="center">
+              <Paper elevation={xs ? 3 : 0} sx={{ p: { xs: 2, md: 0 } }}>
+                <Stack spacing={2}>
+                  {/* 圖片 */}
+                  <Box
+                    sx={{
+                      width: { xs: 300, md: 450 },
+                      height: { xs: 300, md: 450 },
+                    }}
+                  >
+                    <Skeleton
+                      variant="rectangular"
+                      height="100%"
+                      width="100%"
+                    />
+                  </Box>
+                  {/* 名稱 (手機) */}
+                  {xs ? (
+                    <Typography variant="h3" textAlign="center">
+                      商品名稱
+                    </Typography>
+                  ) : null}
+                </Stack>
+              </Paper>
+              {!xs ? <BuyingAgentArea /> : null}
+            </Stack>
+            <Paper elevation={xs ? 3 : 0} sx={{ p: 2, flexGrow: 1 }}>
               <Stack spacing={4}>
+                {/* 名稱 (電腦) */}
+                {!xs ? (
+                  <Typography variant="h1" weightPreset="bold">
+                    商品名稱
+                  </Typography>
+                ) : null}
                 {/* 詳細 */}
                 <DetailItem label="商品價格" value="215NT$" valueBold />
                 <DetailItem label="願付代購費" value="60NT$" valueBold />
@@ -67,7 +132,7 @@ const CommissionDetailPage: NextPage = () => {
                       Forbes Pl Kawerau New Zealand
                     </Stack>
                   }
-                  multiLine
+                  multiLine={xs}
                 />
                 <DetailItem
                   label="送達地點"
@@ -77,45 +142,21 @@ const CommissionDetailPage: NextPage = () => {
                       338 桃園市蘆竹區大興十街6號
                     </Stack>
                   }
-                  multiLine
+                  multiLine={xs}
                 />
                 <DetailItem
                   label="最晚收到商品時間"
                   value="11/15/2022 12:00AM"
-                  multiLine
+                  multiLine={xs}
                 />
                 <DetailItem
                   label="備註"
                   value="請不要造成瓶身撞凹，謝謝"
-                  multiLine
+                  multiLine={xs}
                 />
               </Stack>
             </Paper>
-            <Stack spacing={3}>
-              {/* 代購者 */}
-              <Typography variant="h3" mt={1} sx={{ textAlign: "center" }}>
-                願意代購者
-              </Typography>
-              <BuyingAgent />
-              <BuyingAgent />
-              <Stack alignItems="center">
-                <Link fontSize={18} mt={1}>
-                  顯示更多
-                </Link>
-              </Stack>
-            </Stack>
-            <Stack direction="row" spacing={2} sx={{ "&&": { mt: 5 } }}>
-              {/* 更多代購者 */}
-              <AvatarGroup total={100}>
-                <Avatar />
-                <Avatar />
-                <Avatar />
-                <Avatar />
-              </AvatarGroup>
-              <Typography variant="h6" color="base.400">
-                另有100位代購者的旅途與此委託相符
-              </Typography>
-            </Stack>
+            {xs ? <BuyingAgentArea /> : null}
           </Stack>
         </Stack>
       </Container>
