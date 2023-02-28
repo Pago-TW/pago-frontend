66d3f85ef37c698f96d05dd9faa2fd5d9429a3a2
Kerati <enderwolf50@gmail.com>
2023-03-01 00:00:29 +0800

    build: remove next modularizeImports, again :/

--- next.config.mjs
+++ next.config.mjs
@@ -18,9 +18,6 @@ const config = {
     "@mui/icons-material": {
       transform: "@mui/icons-material/{{member}}",
     },
-    "@mui/material": {
-      transform: "@mui/material/{{member}}",
-    },
     "@mui/lab": {
       transform: "@mui/lab/{{member}}",
     },
