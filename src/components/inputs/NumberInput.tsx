8004b30c8efaf35a3cdd010d99abdb221c0e2eaf
Kerati <enderwolf50@gmail.com>
2023-03-07 19:32:20 +0800

    feat(number-input): new NumberInput component

---
+++ src/components/inputs/NumberInput.tsx
@@ -0,0 +1,40 @@
+import type { TextFieldProps } from "@mui/material";
+import { TextField } from "@mui/material";
+import type { FocusEvent } from "react";
+import { forwardRef } from "react";
+
+export type NumberInputProps = Omit<TextFieldProps, "type">;
+
+export const NumberInput = forwardRef<HTMLInputElement, NumberInputProps>(
+  function NumberInput(
+    { variant = "standard", sx, ...rest }: NumberInputProps,
+    ref
+  ) {
+    const handleFocus = (e: FocusEvent<HTMLInputElement>) => {
+      if (+e.target.value === 0) e.target.select();
+    };
+
+    return (
+      <TextField
+        type="number"
+        variant={variant}
+        sx={{
+          "& input[type=number]::-webkit-inner-spin-button": {
+            WebkitAppearance: "none",
+            margin: 0,
+          },
+          "& input[type=number]::-webkit-outer-spin-button": {
+            WebkitAppearance: "none",
+            margin: 0,
+          },
+          ...sx,
+        }}
+        onFocus={handleFocus}
+        inputRef={ref}
+        {...rest}
+      />
+    );
+  }
+);
+
+export default NumberInput;
