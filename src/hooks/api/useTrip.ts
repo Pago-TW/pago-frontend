e106fdfe6e737a7e805bd8fa85274e087f2f6f75
Kerati <enderwolf50@gmail.com>
2023-03-16 18:08:14 +0800

    feat(/trips/[id]): trip detail page (kind of)
    ***Unresolved***:
     - How to show different page content when visit the page w/ different role
    (consumer/shopper)

---
+++ src/hooks/api/useTrip.ts
@@ -0,0 +1,17 @@
+import type { Trip } from "@/components/TripItem";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const getTrip = async (tripId: Trip["tripId"]): Promise<Trip> => {
+  const res = await axios.get(`/trips/${tripId}`);
+  return res.data;
+};
+
+export const useTrip = (tripId: Trip["tripId"]) => {
+  return useQuery({
+    queryKey: ["trip", tripId],
+    queryFn: () => getTrip(tripId),
+  });
+};
+
+export default useTrip;
