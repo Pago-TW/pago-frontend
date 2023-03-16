21a03dfa82e168f8e6db831073cdde9b7f665656
Kerati <enderwolf50@gmail.com>
2023-03-16 17:41:09 +0800

    fix(trip-list): wrap each mapped TripItem w/ Link

--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -2,6 +2,7 @@ import { Stack } from "@mui/material";
 import { PaperLayout } from "./layouts/PaperLayout";
 import type { Trip } from "./TripItem";
 import { TripItem } from "./TripItem";
+import { Link } from "./ui/Link";
 import { Typography } from "./ui/Typography";
 
 export type TripListProps = {
@@ -15,7 +16,12 @@ export const TripList = ({ category, items }: TripListProps) => {
       <Stack spacing={3}>
         <Typography variant="h5">{category}</Typography>
         {items.map((item) => (
-          <TripItem key={item.tripId} {...item} />
+          <Link
+            key={item.tripId}
+            href={`/trips/${item.tripId}`}
+          >
+            <TripItem key={item.tripId} {...item} />
+          </Link>
         ))}
       </Stack>
     </PaperLayout>
