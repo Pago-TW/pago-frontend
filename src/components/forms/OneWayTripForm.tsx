4a4ef976902c7230ff1f51f071dcd6e6b708b6fb
Kerati <enderwolf50@gmail.com>
2023-03-20 15:34:57 +0800

    feat(/trips/new): finish design & form configure of one way/round trip forms

--- src/components/forms/OneWayTripForm.tsx
+++ src/components/forms/OneWayTripForm.tsx
@@ -1,32 +1,77 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Stack } from "@mui/material";
+import { startOfDay } from "date-fns";
 import { useForm } from "react-hook-form";
-import { CountryInput } from "../inputs/CountryInput";
+import { z } from "zod";
+import { CountryInput, COUNTRY_VALUES } from "../inputs/CountryInput";
+import { DatePicker } from "../inputs/DatePicker";
+import PaperLayout from "../layouts/PaperLayout";
+import { Button } from "../ui/Button";
+
+const currentDate = startOfDay(new Date());
+
+export const oneWayTripFormSchema = z.object({
+  departureCountry: z.enum(COUNTRY_VALUES, {
+    required_error: "請選擇出發地城市",
+  }),
+  destinationCountry: z.enum(COUNTRY_VALUES, {
+    required_error: "請選擇目的地城市",
+  }),
+  departureDate: z.date().min(currentDate, { message: "出發時間不可早於今天" }),
+});
+
+export type OneWayTripFormValues = z.infer<typeof oneWayTripFormSchema>;
+
+export const DEFAULT_VALUES: OneWayTripFormValues = {
+  departureCountry: "新北市",
+  destinationCountry: "臺北市",
+  departureDate: currentDate,
+};
 
 export const OneWayTripForm = () => {
-  const { control } = useForm({
-    defaultValues: {
-      fromCountry: "新北市",
-      toCountry: "新北市",
-
-    },
+  const {
+    control,
+    formState: { errors },
+  } = useForm<OneWayTripFormValues>({
+    mode: "onBlur",
+    defaultValues: DEFAULT_VALUES,
+    resolver: zodResolver(oneWayTripFormSchema),
   });
 
   return (
-    <Stack component="form" spacing={3}>
-      <CountryInput
-        control={control}
-        name="fromCountry"
-        label="出發地城市"
-        FormControlProps={{ fullWidth: true }}
-        SelectProps={{ MenuProps: { sx: { maxHeight: 300 } } }}
-      />
-      <CountryInput
-        control={control}
-        name="toCountry"
-        label="目的地城市"
-        FormControlProps={{ fullWidth: true }}
-        SelectProps={{ MenuProps: { sx: { maxHeight: 300 } } }}
-      />
+    <Stack spacing={3} justifyContent="space-between" height="100%">
+      <PaperLayout>
+        <Stack spacing={3}>
+          <CountryInput
+            control={control}
+            name="departureCountry"
+            label="出發地城市"
+            FormControlProps={{ fullWidth: true }}
+            SelectProps={{ MenuProps: { sx: { maxHeight: 300 } } }}
+          />
+          <CountryInput
+            control={control}
+            name="destinationCountry"
+            label="目的地城市"
+            FormControlProps={{ fullWidth: true }}
+            SelectProps={{ MenuProps: { sx: { maxHeight: 300 } } }}
+          />
+          <DatePicker
+            control={control}
+            name="departureDate"
+            label="出發時間"
+            minDate={currentDate}
+            slotProps={{
+              textField: {
+                error: !!errors.departureDate,
+                helperText: errors.departureDate?.message,
+                fullWidth: true,
+              },
+            }}
+          />
+        </Stack>
+      </PaperLayout>
+      <Button type="submit">新增旅途</Button>
     </Stack>
   );
 };
--- src/components/forms/RoundTripForm.tsx
+++ src/components/forms/RoundTripForm.tsx
@@ -1,64 +1,113 @@
-import { Stack } from "@mui/material";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Stack } from "@mui/material";
+import { startOfDay } from "date-fns";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { CountryInput } from "../inputs/CountryInput";
 import { DatePicker } from "../inputs/DatePicker";
-import { Typography } from "../ui/Typography";
+import PaperLayout from "../layouts/PaperLayout";
+import { Button } from "../ui/Button";
+import { oneWayTripFormSchema } from "./OneWayTripForm";
 
-export const RoundTripForm = () => {
-  const { control, getValues, watch } = useForm({
-    defaultValues: {
-      fromCountry: "新北市",
-      toCountry: "新北市",
-      fromDate: new Date(),
-      toDate: new Date(),
-    },
+const currentDate = startOfDay(new Date());
+
+export const roundTripFormSchema = oneWayTripFormSchema
+  .extend({
+    returnDate: z.date(),
+  })
+  .refine((data) => data.returnDate >= data.departureDate, {
+    message: "返回時間不可早於出發時間",
+    path: ["returnDate"],
   });
 
-  const fromDate = getValues("fromDate");
+export type RoundTripFormValues = z.infer<typeof roundTripFormSchema>;
 
-  console.log(watch());
+export const DEFAULT_VALUES: RoundTripFormValues = {
+  departureCountry: "新北市",
+  destinationCountry: "臺北市",
+  departureDate: currentDate,
+  returnDate: currentDate,
+};
+
+export const RoundTripForm = () => {
+  const {
+    control,
+    watch,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm<RoundTripFormValues>({
+    mode: "onBlur",
+    defaultValues: DEFAULT_VALUES,
+    resolver: zodResolver(roundTripFormSchema),
+  });
 
   return (
-    <Stack component="form" spacing={3}>
-      <CountryInput
-        control={control}
-        name="fromCountry"
-        label="出發地城市"
-        FormControlProps={{ fullWidth: true }}
-        SelectProps={{ MenuProps: { sx: { maxHeight: 300 } } }}
-      />
-      <CountryInput
-        control={control}
-        name="toCountry"
-        label="目的地城市"
-        FormControlProps={{ fullWidth: true }}
-        SelectProps={{ MenuProps: { sx: { maxHeight: 300 } } }}
-      />
-      <Stack direction="row" alignItems="center" spacing={2}>
-        <DatePicker
-          control={control}
-          name="fromDate"
-          label="出發時間"
-          minDate={new Date()}
-          slotProps={{
-            textField: {
-              fullWidth: true,
-            },
-          }}
-        />
-        <Typography>-</Typography>
-        <DatePicker
-          control={control}
-          name="toDate"
-          label="返回時間"
-          minDate={fromDate}
-          slotProps={{
-            textField: {
-              fullWidth: true,
-            },
-          }}
-        />
-      </Stack>
+    <Stack justifyContent="space-between" height="100%">
+      <PaperLayout>
+        <Stack spacing={3}>
+          <CountryInput
+            control={control}
+            name="departureCountry"
+            label="出發地城市"
+            error={!!errors.departureCountry}
+            helperText={errors.departureCountry?.message}
+            FormControlProps={{ fullWidth: true }}
+            SelectProps={{ MenuProps: { sx: { maxHeight: 300 } } }}
+          />
+          <CountryInput
+            control={control}
+            name="destinationCountry"
+            label="目的地城市"
+            error={!!errors.destinationCountry}
+            helperText={errors.destinationCountry?.message}
+            FormControlProps={{ fullWidth: true }}
+            SelectProps={{ MenuProps: { sx: { maxHeight: 300 } } }}
+          />
+          <Stack direction="row" spacing={2}>
+            <DatePicker
+              control={control}
+              name="departureDate"
+              label="出發時間"
+              minDate={currentDate}
+              onChange={(date) => {
+                if (date) {
+                  const returnDate = getValues("returnDate");
+                  if (date > returnDate) setValue("returnDate", date);
+                }
+              }}
+              slotProps={{
+                textField: {
+                  error: !!errors.departureDate,
+                  helperText: errors.departureDate?.message,
+                  fullWidth: true,
+                },
+              }}
+            />
+            <Box
+              display="flex"
+              alignItems="center"
+              pb={!!errors.departureDate || !!errors.returnDate ? 3 : 0}
+            >
+              <span>—</span>
+            </Box>
+            <DatePicker
+              control={control}
+              name="returnDate"
+              label="返回時間"
+              minDate={watch("departureDate")}
+              slotProps={{
+                textField: {
+                  error: !!errors.returnDate,
+                  helperText: errors.returnDate?.message,
+                  fullWidth: true,
+                },
+              }}
+            />
+          </Stack>
+        </Stack>
+      </PaperLayout>
+      <Button type="submit">新增旅途</Button>
     </Stack>
   );
 };
