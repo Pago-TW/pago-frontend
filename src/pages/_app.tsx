238f68b1bdf8aa57bf078b56bd1598fdfe91f190
Kerati <enderwolf50@gmail.com>
2023-04-01 23:52:24 +0800

    refactor(next-auth): no more next-auth, very cool 🫠

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,8 +5,6 @@ import { ThemeProvider } from "@mui/material/styles";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import axios from "axios";
-import type { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { env } from "src/env/client.mjs";
@@ -21,7 +19,7 @@ import "@fontsource/noto-sans-tc/700.css";
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
-interface MyAppProps extends AppProps<{ session: Session | null }> {
+interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
 
@@ -32,22 +30,20 @@ const queryClient = new QueryClient();
 const MyApp = ({
   Component,
   emotionCache = clientSideEmotionCache,
-  pageProps: { session, ...pageProps },
+  pageProps,
 }: MyAppProps) => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <SessionProvider session={session}>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
-          <ReactQueryDevtools />
-        </QueryClientProvider>
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </CacheProvider>
   );
 };
