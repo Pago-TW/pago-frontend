0fc12bb4db108161bf21edf8923554e1163ee7ad
Kerati <enderwolf50@gmail.com>
2023-04-10 10:58:36 +0800

    fix(/auth/signin): migrate sign in page to use next-auth

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -4,14 +4,15 @@ import { Button } from "@/components/ui/Button";
 import { Divider } from "@/components/ui/Divider";
 import { Link } from "@/components/ui/Link";
 import { Typography } from "@/components/ui/Typography";
-import { useToast } from "@/hooks/useToast";
-import { useAuthStore } from "@/store/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import type { SlideProps } from "@mui/material";
-import { Alert, Box, Slide, Snackbar, Stack, TextField } from "@mui/material";
+import { Box, Slide, Stack, TextField } from "@mui/material";
 import type { NextPage } from "next";
+import { signIn } from "next-auth/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
+import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 import { FcGoogle } from "react-icons/fc";
 import { z } from "zod";
@@ -36,15 +37,8 @@ const SlideRightTransition = (props: SlideProps) => {
 };
 
 const SignInPage: NextPage = () => {
-  const {
-    open: toastOpen,
-    message: toastMessage,
-    severity: toastSeverity,
-    openToast,
-    closeToast,
-  } = useToast();
-
   const router = useRouter();
+  const callbackUrl = (router.query.callbackUrl as string) ?? "/";
 
   const {
     register,
@@ -56,27 +50,32 @@ const SignInPage: NextPage = () => {
     resolver: zodResolver(signInFormSchema),
   });
 
-  const signIn = useAuthStore((state) => state.signIn);
+  const { enqueueSnackbar } = useSnackbar();
 
-  const handleSignIn = async (data: SignInFormValues) => {
-    const { error, status, ok } = await signIn(data);
+  const handleGoogleSignIn = useCallback(async () => {
+    await signIn("google", {
+      callbackUrl,
+    });
+  }, [callbackUrl]);
 
-    if (ok) {
-      router.replace("/");
-      openToast({ message: "登入成功", severity: "success" });
-    }
+  const handleCredentialsSignIn = useCallback(
+    async (data: SignInFormValues) => {
+      const res = await signIn("credentials", {
+        email: data.email,
+        password: data.password,
+        callbackUrl,
+        redirect: false,
+      });
 
-    if (error) console.log(error);
-
-    if (!status) {
-      openToast({ message: "未知的錯誤，請洽詢客服", severity: "error" });
-    } else {
-      if (status == 401)
-        openToast({ message: "帳號或密碼錯誤", severity: "error" });
-      else if (status >= 500 && status < 600)
-        openToast({ message: "伺服器錯誤，請洽詢客服", severity: "error" });
-    }
-  };
+      if (res?.error) {
+        enqueueSnackbar(res?.error, {
+          variant: "error",
+          TransitionComponent: SlideRightTransition,
+        });
+      }
+    },
+    [callbackUrl, enqueueSnackbar]
+  );
 
   return (
     <>
@@ -88,14 +87,18 @@ const SignInPage: NextPage = () => {
           <Typography variant="h1" weightPreset="bold" textAlign="center">
             登入
           </Typography>
-          <Button variant="outlined" startIcon={<FcGoogle />}>
+          <Button
+            variant="outlined"
+            startIcon={<FcGoogle />}
+            onClick={handleGoogleSignIn}
+          >
             使用 Google 繼續
           </Button>
           <Divider>或</Divider>
           <Stack
             component="form"
             spacing={3}
-            onSubmit={handleSubmit(handleSignIn)}
+            onSubmit={handleSubmit(handleCredentialsSignIn)}
           >
             <TextField
               variant="outlined"
@@ -124,21 +127,6 @@ const SignInPage: NextPage = () => {
           </Box>
         </Stack>
       </CenterLayout>
-      <Snackbar
-        open={toastOpen}
-        onClose={closeToast}
-        autoHideDuration={5000}
-        TransitionComponent={SlideRightTransition}
-      >
-        <Alert
-          variant="filled"
-          severity={toastSeverity}
-          onClose={closeToast}
-          sx={{ width: "100%" }}
-        >
-          {toastMessage}
-        </Alert>
-      </Snackbar>
     </>
   );
 };
