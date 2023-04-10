5e10d112b59fa34b0d2c0343a9d84197473c9506
Kerati <enderwolf50@gmail.com>
2023-04-11 01:38:21 +0800

    feat(status): add Status component

---
+++ src/components/Status.tsx
@@ -0,0 +1,41 @@
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import React from "react";
+import type { TypographyProps } from "./ui/Typography";
+import { Typography } from "./ui/Typography";
+
+export type StatusCode =
+  | "REQUESTED"
+  | "TO_BE_PURCHASED"
+  | "TO_BE_DELIVERED"
+  | "DELIVERED"
+  | "FINISHED"
+  | "CANCELED";
+
+export type StatusProps = {
+  statusCode: StatusCode;
+};
+
+const statusMap: Record<
+  StatusCode,
+  { text: string; color: TypographyProps["color"] }
+> = {
+  REQUESTED: { text: "待確認", color: "base.main" },
+  TO_BE_PURCHASED: { text: "待購買", color: "secondary.main" },
+  TO_BE_DELIVERED: { text: "待面交", color: "pago.main" },
+  DELIVERED: { text: "已送達", color: "pago.light" },
+  FINISHED: { text: "已完成", color: "success.main" },
+  CANCELED: { text: "不成立", color: "warning.main" },
+};
+
+export const Status: React.FC<StatusProps> = ({ statusCode }) => {
+  const isDesktop = useMediaQuery((theme) => theme.breakpoints.up("md"));
+  const variant = isDesktop ? "h3" : "h6";
+
+  const { text, color } = statusMap[statusCode];
+
+  return (
+    <Typography variant={variant} color={color}>
+      {text}
+    </Typography>
+  );
+};
