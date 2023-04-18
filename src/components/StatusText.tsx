7f45e7793ccb0cf47c03b5c335af92fa3f786eea
Kerati <enderwolf50@gmail.com>
2023-04-19 00:15:09 +0800

    fix(status-text): wrong color code

--- src/components/StatusText.tsx
+++ src/components/StatusText.tsx
@@ -14,23 +14,23 @@ const statusMap: Record<
     REQUESTED: { text: "已發布委託，等待代購者出價", bgColor: "base.400" },
     TO_BE_PURCHASED: {
       text: "媒合成功，代購方請於期限內購買商品",
-      bgColor: "secondary.200",
+      bgColor: "secondary.main",
     },
-    TO_BE_DELIVERED: { text: "商品已購買，等待交付", bgColor: "primary.500" },
-    DELIVERED: { text: "商品已送達", bgColor: "primary.500" },
-    FINISHED: { text: "訂單已完成", bgColor: "success.500" },
-    CANCELED: { text: "此訂單不成立", bgColor: "warning.500" },
+    TO_BE_DELIVERED: { text: "商品已購買，等待交付", bgColor: "primary.main" },
+    DELIVERED: { text: "商品已送達", bgColor: "primary.main" },
+    FINISHED: { text: "訂單已完成", bgColor: "success.main" },
+    CANCELED: { text: "此訂單不成立", bgColor: "warning.main" },
   },
   shopper: {
     REQUESTED: { text: "已向委託者出價，等待委託者回應", bgColor: "base.400" },
     TO_BE_PURCHASED: {
       text: "媒合成功，代購方請於期限內購買商品",
-      bgColor: "secondary.200",
+      bgColor: "secondary.main",
     },
-    TO_BE_DELIVERED: { text: "商品已購買，等待交付", bgColor: "primary.500" },
-    DELIVERED: { text: "商品已送達", bgColor: "primary.500" },
-    FINISHED: { text: "訂單已完成", bgColor: "success.500" },
-    CANCELED: { text: "此訂單不成立", bgColor: "warning.500" },
+    TO_BE_DELIVERED: { text: "商品已購買，等待交付", bgColor: "primary.main" },
+    DELIVERED: { text: "商品已送達", bgColor: "primary.main" },
+    FINISHED: { text: "訂單已完成", bgColor: "success.main" },
+    CANCELED: { text: "此訂單不成立", bgColor: "warning.main" },
   },
 };
 
