8a084f4c1daf6323d1bb02488041162a79bcd7ae
Kerati <enderwolf50@gmail.com>
2023-04-25 23:10:54 +0800

    refactor(actions): add a generic component ActionWithModalAndConfirmation
    I know this name is long but BRUH TS IS SO HARD

--- src/components/Actions.tsx
+++ src/components/Actions.tsx
@@ -11,7 +11,13 @@ import type { StatusCode } from "@/types/order";
 import { Box, Paper } from "@mui/material";
 import type { ConfirmOptions } from "material-ui-confirm";
 import { ConfirmProvider, useConfirm } from "material-ui-confirm";
-import { type ReactNode } from "react";
+import type { ComponentType, ReactNode } from "react";
+
+const DEFAULT_CONFIRM_OPTIONS: ConfirmOptions = {
+  cancellationText: "取消",
+  confirmationText: "確定",
+  buttonOrder: ["confirm", "cancel"],
+};
 
 const ActionsWrapper = ({ children }: { children: ReactNode }) => {
   const isDesktop = useMediaQuery((theme) => theme.breakpoints.up("md"));
@@ -52,6 +58,7 @@ type ActionButtonProps = Pick<
   ButtonProps,
   "variant" | "color" | "disabled" | "children"
 > & { onClick?: () => void };
+
 const ActionButton = (props: ActionButtonProps) => {
   const isDesktop = useMediaQuery((theme) => theme.breakpoints.up("md"));
 
@@ -76,12 +83,6 @@ type ActionWithConfirmationProps = Omit<ActionButtonProps, "onClick"> & {
   confirmOptions: ConfirmOptions;
 };
 
-const DEFAULT_CONFIRM_OPTIONS: ConfirmOptions = {
-  cancellationText: "取消",
-  confirmationText: "確定",
-  buttonOrder: ["confirm", "cancel"],
-};
-
 const ActionWithConfirmation = (props: ActionWithConfirmationProps) => {
   const { confirmOptions, onClick, children, ...rest } = props;
 
@@ -98,6 +99,46 @@ const ActionWithConfirmation = (props: ActionWithConfirmationProps) => {
     <ActionButton onClick={handleClick} {...rest}>
       {children}
     </ActionButton>
+  );
+};
+
+type ActionWIthModalAndConfirmationProps<T extends object = object> = Omit<
+  ActionWithConfirmationProps,
+  "onClick"
+> & {
+  onClick: (data: T) => void;
+  Modal: ComponentType<{
+    open: boolean;
+    onClose: () => void;
+    onSubmit: (data: T) => void;
+  }>;
+};
+
+const ActionWithModalAndConfirmation = <T extends object>(
+  props: ActionWIthModalAndConfirmationProps<T>
+) => {
+  const { confirmOptions, onClick, Modal, children, ...rest } = props;
+
+  const { open, handleOpen, handleClose } = useOpen();
+  const confirm = useConfirm();
+
+  const handleClick = async () => handleOpen();
+
+  const handleSubmit = async (data: T) => {
+    try {
+      handleClose();
+      await confirm({ ...DEFAULT_CONFIRM_OPTIONS, ...confirmOptions });
+      if (onClick) onClick(data);
+    } catch {}
+  };
+
+  return (
+    <>
+      <ActionButton onClick={handleClick} {...rest}>
+        {children}
+      </ActionButton>
+      <Modal open={open} onClose={handleClose} onSubmit={handleSubmit} />
+    </>
   );
 };
 
@@ -137,84 +178,62 @@ const EditAction = (
   );
 };
 
-const CancelAction = (props: {
-  disabled?: boolean;
-  onClick: (data: CancelFormValues) => void;
-}) => {
-  const { disabled, onClick } = props;
+const CancelAction = (
+  props: Pick<
+    ActionWIthModalAndConfirmationProps<CancelFormValues>,
+    "disabled" | "onClick"
+  > & {
+    perspective: Perspective;
+  }
+) => {
+  const { disabled, onClick, perspective } = props;
 
-  const { open, handleOpen, handleClose } = useOpen();
-  const confirm = useConfirm();
-
-  const handleClick = async () => {
-    handleOpen();
-  };
-  const handleSubmit = async (data: CancelFormValues) => {
-    try {
-      handleClose();
-      await confirm({ ...DEFAULT_CONFIRM_OPTIONS, title: "確定取消此代購？" });
-      if (onClick) onClick(data);
-    } catch {}
-  };
+  const handleSubmit = async (data: CancelFormValues) => onClick(data);
 
   return (
-    <>
-      <ActionButton
+    <ActionWithModalAndConfirmation
         variant="outlined"
         color="error"
-        onClick={handleClick}
         disabled={disabled}
+      confirmOptions={{ title: "確定取消此代購？" }}
+      onClick={handleSubmit}
+      Modal={CancelModal}
       >
-        取消委託
-      </ActionButton>
-      <CancelModal open={open} onClose={handleClose} onSubmit={handleSubmit} />
-    </>
+      {perspective === "consumer" ? "取消委託" : "取消代購"}
+    </ActionWithModalAndConfirmation>
   );
 };
 
-const PostponeAction = (props: {
-  disabled?: boolean;
-  onClick: (data: PostponeFormValues) => void;
-}) => {
+const PostponeAction = (
+  props: Pick<
+    ActionWIthModalAndConfirmationProps<PostponeFormValues>,
+    "disabled" | "onClick"
+  >
+) => {
   const { disabled, onClick } = props;
 
-  const { open, handleOpen, handleClose } = useOpen();
-  const confirm = useConfirm();
-
-  const handleClick = async () => {
-    handleOpen();
-  };
-  const handleSubmit = async (data: PostponeFormValues) => {
-    try {
-      handleClose();
-      await confirm({ ...DEFAULT_CONFIRM_OPTIONS, title: "確定申請延期？" });
-      if (onClick) onClick(data);
-    } catch {}
-  };
+  const handleSubmit = async (data: PostponeFormValues) => onClick(data);
 
   return (
-    <>
-      <ActionButton
+    <ActionWithModalAndConfirmation
         variant="outlined"
         color="error"
-        onClick={handleClick}
         disabled={disabled}
+      confirmOptions={{ title: "確定申請延期？" }}
+      onClick={handleSubmit}
+      Modal={PostponeModal}
       >
         申請延期
-      </ActionButton>
-      <PostponeModal
-        open={open}
-        onClose={handleClose}
-        onSubmit={handleSubmit}
-      />
-    </>
+    </ActionWithModalAndConfirmation>
   );
 };
 
 const FinishAction = (
-  props: Pick<ActionWithConfirmationProps, "disabled" | "onClick">
+  props: Pick<ActionWithConfirmationProps, "disabled" | "onClick"> & {
+    perspective: Perspective;
+  }
 ) => {
-  const { disabled, onClick } = props;
+  const { disabled, onClick, perspective } = props;
 
   return (
     <ActionWithConfirmation
@@ -222,7 +241,7 @@ const FinishAction = (
       confirmOptions={{ title: "確定完成此訂單？" }}
       onClick={onClick}
     >
-      完成委託
+      {perspective === "consumer" ? "完成委託" : "完成代購"}
     </ActionWithConfirmation>
   );
 };
@@ -251,7 +270,7 @@ export const Actions = (props: ActionsProps) => {
   if (perspective === "consumer") {
     const disabled =
       statusCode === "TO_BE_CANCELLED" || statusCode === "TO_BE_POSTPONED";
-
+  if (perspective === "consumer") {
     switch (statusCode) {
       case "REQUESTED":
         return (
@@ -264,7 +283,11 @@ export const Actions = (props: ActionsProps) => {
       case "TO_BE_CANCELLED":
         return (
           <ActionsWrapper>
-            <CancelAction disabled={disabled} onClick={onApplyCancel} />
+            <CancelAction
+              disabled={disabled}
+              onClick={onApplyCancel}
+              perspective={perspective}
+            />
             <PostponeAction disabled={disabled} onClick={onApplyPostpone} />
           </ActionsWrapper>
         );
@@ -274,7 +297,11 @@ export const Actions = (props: ActionsProps) => {
         return (
           <ActionsWrapper>
             <PostponeAction disabled={disabled} onClick={onApplyPostpone} />
-            <FinishAction disabled={disabled} onClick={onFinish} />
+            <FinishAction
+              disabled={disabled}
+              onClick={onFinish}
+              perspective={perspective}
+            />
           </ActionsWrapper>
         );
       case "FINISHED":
