a044d0c0bd4b9123688bb9ae3af7cc27ffe91203
Kerati <enderwolf50@gmail.com>
2023-04-27 23:06:18 +0800

    feat(types/util): add `AtLeastOneRequired` & `OmitWithSubstring` util types which idk if i will use

--- src/types/util.ts
+++ src/types/util.ts
@@ -1,2 +1,14 @@
 export type WithRequired<T, K extends keyof T> = Omit<T, K> &
   Required<Pick<T, K>>;
+
+export type AtLeastOneRequired<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
+
+export type OmitWithSubstring<T extends object, S extends string> = {
+  [K in keyof T as Exclude<K, `${string}${S}${string}`>]: T[K];
+};
