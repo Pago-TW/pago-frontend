fdc14fadbed4f71e43ff0ae3bf45173da12615f2
Kerati <enderwolf50@gmail.com>
2023-05-02 00:27:57 +0800

    feat(hooks/add-user): add useUser hook to get user info

---
+++ src/hooks/api/useUser.ts
@@ -0,0 +1,15 @@
+import { axios } from "@/libs/axios";
+import type { User } from "@/types/user";
+import { useQuery } from "@tanstack/react-query";
+
+const getUser = async (userId: User["userId"]) => {
+  const res = await axios.get<User>(`/users/${userId}`);
+  return res.data;
+};
+
+export const useUser = (userId: User["userId"]) => {
+  return useQuery({
+    queryKey: ["user", userId],
+    queryFn: () => getUser(userId),
+  });
+};
