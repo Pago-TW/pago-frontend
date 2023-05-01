9aa8597b59c7a7dff088d0291e387b6e72bc4fcb
Kerati <enderwolf50@gmail.com>
2023-05-02 00:27:32 +0800

    fix(flatten-infinite-paginated-data): return a empty array if mapped result is undefined

--- src/utils/flattenInfinitePaginatedData.ts
+++ src/utils/flattenInfinitePaginatedData.ts
@@ -4,5 +4,5 @@ import type { InfiniteData } from "@tanstack/react-query";
 export const flattenInfinitePaginatedData = <T extends object>(
   data?: InfiniteData<PaginatedResponse<T[]>>
 ) => {
-  return data?.pages?.flatMap((page) => page.data);
+  return data?.pages?.flatMap((page) => page.data) ?? [];
 };
