dd78e18e60bb29cbd0dc0b5acc8c101a797d1d6d
Kerati <enderwolf50@gmail.com>
2023-05-03 19:50:41 +0800

    feat(tab-list): add styled TabList to justify all things center

---
+++ src/components/ui/TabList.tsx
@@ -0,0 +1,8 @@
+import { TabList as MuiTabList } from "@mui/lab";
+import { styled } from "@mui/material";
+
+export const TabList = styled(MuiTabList)({
+  "& .MuiTabs-flexContainer": {
+    justifyContent: "center",
+  },
+});
