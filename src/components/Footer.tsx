243cb8ba6fe6f91fc03c1cfeddb8ea352124482b
shiun0413 <a0923183408@gmail.com>
2023-05-04 04:50:49 +0800

    refactor(Footer): Adjust footer layout for mobile and desktop views

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,105 +1,149 @@
 // Footer.tsx
 import { Link } from "@/components/ui/Link";
 import { Typography } from "@/components/ui/Typography";
-import { Box, Stack } from "@mui/material";
+import { Box, Stack, Grid } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GoogleIcon from "@mui/icons-material/Google";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
 import React from "react";
 
 const Footer: React.FC = () => {
   return (
     <Box
       sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
+        display: "grid",
+        gridTemplateRows: { xs: "auto 1fr", md: "1fr" },
+        gridTemplateColumns: { xs: "1fr", md: "repeat(3, 1fr)" },
+        alignItems: { xs: "center", md: "start" },
         justifyContent: "center",
         padding: 2,
         backgroundColor: (theme) => theme.palette.background.paper,
       }}
     >
-      <Box sx={{ paddingBottom: 5 }}>
-        <Typography
-          variant="h3"
-          color="primary.main"
-          weightPreset="bold"
-          textAlign={{ xs: "center", md: "left" }}
-          sx={{ paddingBottom: 2 }}
-        >
-          Pago
-        </Typography>
+      <Box
+        sx={{
+          paddingBottom: { xs: 5, md: 0 },
+          paddingRight: { md: 5 },
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Stack spacing={2} alignItems="flex-start">
+          <Box>
+            <Typography
+              variant="h3"
+              color="primary.main"
+              weightPreset="bold"
+              textAlign={"center"}
+            >
+              Pago
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" color="base.500" weightPreset="normal">
+              <Stack spacing={2}>
+                <Link href="#">會員中心</Link>
+                <Link href="#">我的委託</Link>
+                <Link href="#">旅途管理</Link>
+                <Link href="#">設定</Link>
+                <Link href="#">我的收藏</Link>
+                <Link href="#">關於我們</Link>
+                <Link href="#">幫助中心</Link>
+              </Stack>
+            </Typography>
+          </Box>
+        </Stack>
+      </Box>
+      <Box
+        sx={{
+          paddingBottom: { xs: 5, md: 0 },
+          paddingRight: { md: 5 },
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Stack spacing={2} alignItems="flex-start">
+          <Box>
+            <Typography
+              variant="h3"
+              color="primary.main"
+              weightPreset="bold"
+              textAlign={"center"}
+            >
+              相關連結
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" color="base.500" weightPreset="normal">
+              <Link href="#">
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <InstagramIcon />
+                  <Box sx={{ marginLeft: "9px" }}>Instagram</Box>
+                </Box>
+              </Link>
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" color="base.500" weightPreset="normal">
+              <Link href="#">
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <GoogleIcon />
+                  <Box sx={{ marginLeft: "9px" }}>Google</Box>
+                </Box>
+              </Link>
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" color="base.500" weightPreset="normal">
+              <Link href="#">
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <FacebookRoundedIcon />
+                  <Box sx={{ marginLeft: "9px" }}>Facebook</Box>
+                </Box>
+              </Link>
+            </Typography>
+          </Box>
+        </Stack>
+      </Box>
 
-        <Typography
-          variant="h6"
-          color="base.500"
-          weightPreset="normal"
-          textAlign={{ xs: "center", md: "left" }}
-          sx={{ display: "flex", justifyContent: "center" }}
-        >
-          <Stack spacing={2}>
-            <Link href="#">會員中心</Link>
-            <Link href="#">我的委託</Link>
-            <Link href="#">旅途管理</Link>
-            <Link href="#">設定</Link>
-            <Link href="#">我的收藏</Link>
-            <Link href="#">關於我們</Link>
-            <Link href="#">幫助中心</Link>
-          </Stack>
-        </Typography>
+      <Box
+        sx={{
+          paddingBottom: 5,
+          paddingRight: { md: 5 },
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Stack spacing={2} alignItems="flex-start">
+          <Box>
+            <Typography
+              variant="h3"
+              color="primary.main"
+              weightPreset="bold"
+              textAlign={"center"}
+            >
+              用戶權益
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="h6" color="base.500" weightPreset="normal">
+              <Stack spacing={2}>
+                <Link href="#">使用者條款</Link>
+                <Link href="#">隱私權政策</Link>
+              </Stack>
+            </Typography>
+          </Box>
+        </Stack>
       </Box>
-      <Box sx={{ paddingBottom: 5 }}>
-        <Typography
-          variant="h3"
-          color="primary.main"
-          weightPreset="bold"
-          textAlign={{ xs: "center", md: "left" }}
-          sx={{ paddingBottom: 2 }}
-        >
-          相關連結
-        </Typography>
-        <Typography
-          variant="h6"
-          color="base.500"
-          weightPreset="normal"
-          textAlign={{ xs: "center", md: "left" }}
-          sx={{ display: "flex", justifyContent: "center" }}
-        >
-          <Stack spacing={2}>
-            <Link href="#">Instagram</Link>
-            <Link href="#">Google</Link>
-            <Link href="#">Facebook</Link>
-          </Stack>
-        </Typography>
-      </Box>
-      <Box sx={{ paddingBottom: 5 }}>
-        <Typography
-          variant="h3"
-          color="primary.main"
-          weightPreset="bold"
-          textAlign={{ xs: "center", md: "left" }}
-          sx={{ paddingBottom: 2 }}
-        >
-          用戶權益
-        </Typography>
-        <Typography
-          variant="h6"
-          color="base.500"
-          weightPreset="normal"
-          textAlign={{ xs: "center", md: "left" }}
-          sx={{ display: "flex", justifyContent: "center" }}
-        >
-          <Stack spacing={2}>
-            <Link href="#">使用者條款</Link>
-            <Link href="#">隱私權政策</Link>
-          </Stack>
-        </Typography>
-      </Box>
-      <Typography
+      {/* <Typography
         variant="h6"
         color="primary.main"
         weightPreset="normal"
         textAlign={{ xs: "center", md: "left" }}
+        sx={{ gridRow: { xs: "2", md: "1" }, gridColumn: { md: "span 3" } }}
       >
         Copyright © 2023 Pago
-      </Typography>
+      </Typography> */}
     </Box>
   );
 };
