4841069f696998749d560c178093117b847bac0a
Kerati <enderwolf50@gmail.com>
2023-05-03 19:14:11 +0800

    refactor(trip-list): let TripList only focus on trips display

--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -1,27 +1,23 @@
 import type { Trip } from "@/types/trip";
+import type { StackProps } from "@mui/material";
 import { Stack } from "@mui/material";
 import { TripItem } from "./TripItem";
-import { PaperLayout } from "./layouts/PaperLayout";
 import { Link } from "./ui/Link";
-import { Typography } from "./ui/Typography";
 
 export type TripListProps = {
-  category: string;
   items: Trip[];
+  spacing?: StackProps["spacing"];
 };
 
-export const TripList = ({ category, items }: TripListProps) => {
+export const TripList = ({ spacing = 3, items }: TripListProps) => {
   return (
-    <PaperLayout>
-      <Stack spacing={3}>
-        <Typography variant="h5">{category}</Typography>
-        {items.map((item) => (
-          <Link key={item.tripId} href={`/trips/${item.tripId}`}>
-            <TripItem key={item.tripId} {...item} />
-          </Link>
-        ))}
-      </Stack>
-    </PaperLayout>
+    <Stack spacing={spacing}>
+      {items.map((item) => (
+        <Link key={item.tripId} href={`/trips/${item.tripId}`}>
+          <TripItem key={item.tripId} {...item} />
+        </Link>
+      ))}
+    </Stack>
   );
 };
 
--- src/pages/trips/index.tsx
+++ src/pages/trips/index.tsx
@@ -1,8 +1,11 @@
 import { BaseLayout } from "@/components/layouts/BaseLayout";
 import { PageTitle } from "@/components/PageTitle";
+import type { TripListProps } from "@/components/TripList";
 import { TripList } from "@/components/TripList";
 import { Button } from "@/components/ui/Button";
 import { Link } from "@/components/ui/Link";
+import { Paper } from "@/components/ui/Paper";
+import { Typography } from "@/components/ui/Typography";
 import { useTrips } from "@/hooks/api/useTrips";
 import { flattenInfinitePaginatedData } from "@/utils/flattenInfinitePaginatedData";
 import { Add } from "@mui/icons-material";
@@ -10,7 +13,24 @@ import { Container, Stack } from "@mui/material";
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
+import type { FC } from "react";
 import { useMemo } from "react";
+
+type CategorizedTripListProps = TripListProps & { category: string };
+
+const CategorizedTripList: FC<CategorizedTripListProps> = ({
+  category,
+  ...tripListProps
+}) => {
+  return (
+    <Paper sx={{ p: 2 }}>
+      <Stack spacing={3}>
+        <Typography variant="h5">{category}</Typography>
+        <TripList {...tripListProps} />
+      </Stack>
+    </Paper>
+  );
+};
 
 const TripsPage: NextPage = () => {
   const { data: session } = useSession();
@@ -37,9 +57,9 @@ const TripsPage: NextPage = () => {
         </PageTitle>
         <Container>
           <Stack spacing={2}>
-            <TripList category="即將出發" items={trips} />
-            <TripList category="正在途中" items={trips} />
-            <TripList category="歷史旅途" items={trips} />
+            <CategorizedTripList category="即將出發" items={trips} />
+            <CategorizedTripList category="正在途中" items={trips} />
+            <CategorizedTripList category="歷史旅途" items={trips} />
           </Stack>
         </Container>
       </BaseLayout>
