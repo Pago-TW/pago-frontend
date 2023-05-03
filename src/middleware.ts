39e456312d7783d2902022d1223ccf6742d19b35
Kerati <enderwolf50@gmail.com>
2023-05-03 19:58:51 +0800

    feat(middleware): add new matcher rule to match any url starts with /users/me

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/orders/:path*", "/trips/:path*"],
+  matcher: ["/orders/:path*", "/trips/:path*", "/users/me/:path*"],
 };
