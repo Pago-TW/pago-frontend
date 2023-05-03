32b3a905af247a48149e465cb5e6b6ab27fd7d27
Kerati <enderwolf50@gmail.com>
2023-05-03 19:20:01 +0800

    feat(use-user-me): add useUserMe hook to get info of current logged in user

---
+++ src/hooks/api/useUserMe.ts
@@ -0,0 +1,15 @@
+import { axios } from "@/libs/axios";
+import type { User } from "@/types/user";
+import { useQuery } from "@tanstack/react-query";
+
+const getUserMe = async () => {
+  const res = await axios.get<User>("/users/me");
+  return res.data;
+};
+
+export const useUserMe = () => {
+  return useQuery({
+    queryKey: ["users", "me"],
+    queryFn: getUserMe,
+  });
+};
