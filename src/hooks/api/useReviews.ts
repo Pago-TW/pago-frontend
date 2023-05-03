9f089069239d3b03a28559c81045a2c04f4a159e
Kerati <enderwolf50@gmail.com>
2023-05-03 19:21:03 +0800

    feat(use-reviews): add useReviews hook to get reviews from the given user

---
+++ src/hooks/api/useReviews.ts
@@ -0,0 +1,40 @@
+import { axios } from "@/libs/axios";
+import type { PaginatedResponse, PaginationParams } from "@/types/api";
+import type { Review, ReviewType } from "@/types/review";
+import type { User } from "@/types/user";
+import { getLastIndex } from "@/utils/getLastIndex";
+import { useInfiniteQuery } from "@tanstack/react-query";
+
+type Params = PaginationParams<{
+  type: ReviewType;
+}>;
+
+type Options = {
+  params?: Params;
+  pageParam?: number;
+};
+
+const getReviews = async (userId: User["userId"], options: Options = {}) => {
+  const { params, pageParam } = options;
+
+  const res = await axios.get<PaginatedResponse<Review[]>>("/reviews", {
+    params: {
+      startIndex: pageParam,
+      size: 10,
+      userId,
+      ...params,
+    },
+  });
+  return res.data;
+};
+
+export const useReviews = (userId: User["userId"], params?: Params) => {
+  return useInfiniteQuery({
+    queryKey: ["reviews", userId, params],
+    queryFn: ({ pageParam }) => getReviews(userId, { params, pageParam }),
+    getNextPageParam: (lastPage) => {
+      const lastIndex = getLastIndex(lastPage);
+      return lastIndex < lastPage.total ? lastIndex + 1 : undefined;
+    },
+  });
+};
