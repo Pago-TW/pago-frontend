bac7e23880615191e46a5806c8531d97e54b9425
Kerati <enderwolf50@gmail.com>
2023-05-07 20:16:48 +0800

    feat(/trips): categorize trips

--- src/pages/trips/index.tsx
+++ src/pages/trips/index.tsx
@@ -7,42 +7,51 @@ import { Link } from "@/components/ui/Link";
 import { Paper } from "@/components/ui/Paper";
 import { Typography } from "@/components/ui/Typography";
 import { useTrips } from "@/hooks/api/useTrips";
+import type { TripStatus } from "@/types/trip";
 import { flattenInfinitePaginatedData } from "@/utils/flattenInfinitePaginatedData";
 import { Add } from "@mui/icons-material";
 import { Container, Stack } from "@mui/material";
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
-import type { FC } from "react";
-import { useMemo } from "react";
+import { useMemo, type FC } from "react";
 
-type CategorizedTripListProps = TripListProps & { category: string };
+type CategorizedTripListProps = Omit<TripListProps, "items"> & {
+  category: string;
+  status: TripStatus;
+};
 
 const CategorizedTripList: FC<CategorizedTripListProps> = ({
   category,
+  status,
   ...tripListProps
 }) => {
+  const { data: session } = useSession();
+
+  const userId = session?.user?.id;
+  const { data: tripsData } = useTrips(
+    { userId, status },
+    { enabled: !!userId }
+  );
+
+  const trips = useMemo(
+    () => flattenInfinitePaginatedData(tripsData),
+    [tripsData]
+  );
+
   return (
     <Paper sx={{ p: 2 }}>
       <Stack spacing={3}>
         <Typography variant="h5">{category}</Typography>
-        <TripList {...tripListProps} />
+        {trips.length !== 0 ? (
+          <TripList items={trips} {...tripListProps} />
+        ) : null}
       </Stack>
     </Paper>
   );
 };
 
 const TripsPage: NextPage = () => {
-  const { data: session } = useSession();
-
-  const userId = session?.user?.id;
-  const { data: tripsData } = useTrips({ userId }, { enabled: !!userId });
-
-  const trips = useMemo(
-    () => flattenInfinitePaginatedData(tripsData) ?? [],
-    [tripsData]
-  );
-
   return (
     <>
       <Head>
@@ -57,9 +66,9 @@ const TripsPage: NextPage = () => {
         </PageTitle>
         <Container>
           <Stack spacing={2}>
-            <CategorizedTripList category="即將出發" items={trips} />
-            <CategorizedTripList category="正在途中" items={trips} />
-            <CategorizedTripList category="歷史旅途" items={trips} />
+            <CategorizedTripList category="即將出發" status="UPCOMING" />
+            <CategorizedTripList category="正在途中" status="ONGOING" />
+            <CategorizedTripList category="歷史旅途" status="PAST" />
           </Stack>
         </Container>
       </BaseLayout>
