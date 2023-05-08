ab90c9e8e9d0f516482893db9f5ae5fb2a24f099
Kerati <enderwolf50@gmail.com>
2023-05-08 23:51:01 +0800

    refactor(use-locale): use useEffect in useLocale hook to make it compatible with SSR

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,4 +1,32 @@
+import { useEffect, useState } from "react";
+
+declare global {
+  interface Navigator {
+    userLanguage?: string;
+  }
+}
+
+const getLocale = () => {
+  let locale =
+    (typeof navigator !== "undefined" &&
+      (navigator.language || navigator.userLanguage)) ||
+    "en-US";
+
+  try {
+    Intl.DateTimeFormat.supportedLocalesOf([locale]);
+  } catch (_err) {
+    locale = "en-US";
+  }
+
+  return locale;
+};
+
 export const useLocale = () => {
-  const locale = navigator.language || "zh-TW";
+  const [locale, setLocale] = useState(getLocale());
+
+  useEffect(() => {
+    setLocale(getLocale());
+  }, []);
+
   return locale;
 };
