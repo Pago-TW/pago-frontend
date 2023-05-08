93c1b6191bcb0982c026bb5d89d93f3cedc68a5c
Kerati <enderwolf50@gmail.com>
2023-05-08 23:59:51 +0800

    build(devdeps): add @next/bundle-analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,40 @@
+import bundleAnalyzer from "@next/bundle-analyzer";
 import { withPlaiceholder } from "@plaiceholder/next";
 import "./src/env.mjs";
 
-/** @type {import("next").NextConfig} */
-const config = withPlaiceholder({
-  reactStrictMode: true,
-  swcMinify: true,
-  i18n: {
-    locales: ["zh-TW"],
-    defaultLocale: "zh-TW",
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "pago-file-storage.s3.ap-northeast-1.amazonaws.com",
-        port: "",
-        pathname: "**",
-      },
-    ],
-  },
-  transpilePackages: ["@mui/system", "@mui/material", "@mui/icons-material"],
-  modularizeImports: {
-    "@mui/icons-material": {
-      transform: "@mui/icons-material/{{member}}",
-    },
-    "@mui/lab": {
-      transform: "@mui/lab/{{member}}",
-    },
-  },
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
 });
+
+/** @type {import("next").NextConfig} */
+const config = withBundleAnalyzer(
+  withPlaiceholder({
+    reactStrictMode: true,
+    swcMinify: true,
+    i18n: {
+      locales: ["zh-TW"],
+      defaultLocale: "zh-TW",
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "pago-file-storage.s3.ap-northeast-1.amazonaws.com",
+          port: "",
+          pathname: "**",
+        },
+      ],
+    },
+    transpilePackages: ["@mui/system", "@mui/material", "@mui/icons-material"],
+    modularizeImports: {
+      "@mui/icons-material": {
+        transform: "@mui/icons-material/{{member}}",
+      },
+      "@mui/lab": {
+        transform: "@mui/lab/{{member}}",
+      },
+    },
+  })
+);
 
 export default config;
