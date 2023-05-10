6c0254f2a4be1f19e0f7d823ec4fddf35c2e1b90
shiun0413 <a0923183408@gmail.com>
2023-05-11 01:37:09 +0800

    feat(hooks/api): add useRequestPasswordReset.ts which implements API `POST /auth/request-password-reset`

---
+++ src/hooks/api/useRequestPasswordReset.ts
@@ -0,0 +1,28 @@
+import { axios } from "@/libs/axios";
+import { useMutation } from "@tanstack/react-query";
+
+type RequestPasswordResetData = {
+  email: string;
+};
+
+type RequestPasswordResetResponse = {
+  passwordResetTokenId: string;
+  userId: string;
+  token: string;
+  expiryDate: string;
+  createDate: string;
+};
+
+const requestPasswordReset = async (
+  data: RequestPasswordResetData
+): Promise<RequestPasswordResetResponse> => {
+  const res = await axios.post<RequestPasswordResetResponse>(
+    "/auth/request-password-reset",
+    data
+  );
+  return res.data;
+};
+
+export const useRequestPasswordReset = () => {
+  return useMutation({ mutationFn: requestPasswordReset });
+};
