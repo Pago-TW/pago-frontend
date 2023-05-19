d19a98e6697aee7b0d522f54727033f75bcde21b
Kerati <enderwolf50@gmail.com>
2023-05-20 00:02:38 +0800

    refactor(phone-input): add phone schema

--- src/components/inputs/PhoneInput.tsx
+++ src/components/inputs/PhoneInput.tsx
@@ -3,6 +3,12 @@ import { TextField } from "@mui/material";
 import { forwardRef, type FC } from "react";
 import type { PatternFormatProps } from "react-number-format";
 import { PatternFormat } from "react-number-format";
+import { z } from "zod";
+
+export const phoneSchema = z
+  .string()
+  .trim()
+  .regex(/^09\d{8}$/, { message: "無效的電話號碼" });
 
 export type PhoneInputProps = Omit<
   PatternFormatProps<TextFieldProps>,
