7a3f16ceee022f49953b92ff9d3b19200afa0bf5
Kerati <enderwolf50@gmail.com>
2023-05-20 00:04:47 +0800

    feat(hooks): add hooks to trigger sns and verify otp

---
+++ src/hooks/api/useVerifyOtp.ts
@@ -0,0 +1,23 @@
+import { axios } from "@/libs/axios";
+import { useMutation } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
+
+type VerifyOtpData = {
+  phone: string;
+  otpCode: string;
+};
+
+const verifyOtp = async (data: VerifyOtpData) => {
+  const res = await axios.post<boolean>("/otp/validate", data);
+  return res.data;
+};
+
+export const useVerifyOtp = () => {
+  const { update } = useSession();
+  return useMutation({
+    mutationFn: verifyOtp,
+    onSuccess: (data) => {
+      if (data) update();
+    },
+  });
+};
