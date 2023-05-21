78bdf9792bb1d6aa99df64914c3e377562ace917
Kerati <enderwolf50@gmail.com>
2023-05-21 19:08:35 +0800

    feat(take-order-action): show snackbar when non verified user tried to take order

--- src/components/actions/TakeOrderAction.tsx
+++ src/components/actions/TakeOrderAction.tsx
@@ -1,9 +1,13 @@
 import { useAddBid } from "@/hooks/api/useAddBid";
 import { useOpen } from "@/hooks/useOpen";
 import { useConfirm } from "material-ui-confirm";
+import { useSession } from "next-auth/react";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
+import { useSnackbar } from "notistack";
 import type { FC } from "react";
+import { CloseSnackbarButton } from "../CloseSnackbarButton";
+import { Button } from "../ui/Button";
 import { ActionButton } from "./ActionButton";
 import type { TakeOrderFormValues } from "./TakeOrderPopup";
 
@@ -15,12 +19,43 @@ export const TakeOrderAction: FC = () => {
   const router = useRouter();
   const orderId = router.query.orderId as string;
 
+  const { data: session } = useSession();
+
   const { open, handleOpen, handleClose } = useOpen();
 
   const confirm = useConfirm();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   const { mutate: addBid } = useAddBid();
 
+  const handleActionClick = async () => {
+    if (session?.user?.verified) {
+      handleOpen();
+      return;
+    }
+
+    enqueueSnackbar({
+      message: "尚未綁定銀行帳戶",
+      variant: "warning",
+      action: (key) => (
+        <>
+          <Button
+            variant="text"
+            onClick={() => {
+              router.push({
+                pathname: "/users/me/payments/new",
+                query: { redirectUrl: router.asPath },
+              });
+              closeSnackbar(key);
+            }}
+          >
+            前往綁定
+          </Button>
+          <CloseSnackbarButton snackbarKey={key} />
+        </>
+      ),
+    });
+  };
   const handleSubmit = async (data: TakeOrderFormValues) => {
     try {
       await confirm({
@@ -42,7 +77,7 @@ export const TakeOrderAction: FC = () => {
 
   return (
     <>
-      <ActionButton onClick={handleOpen}>接受委託</ActionButton>
+      <ActionButton onClick={handleActionClick}>接受委託</ActionButton>
       <DynamicTakeOrderPopup
         orderId={orderId}
         open={open}
