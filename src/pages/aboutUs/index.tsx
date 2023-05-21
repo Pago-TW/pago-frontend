b288838a75383369627d59f25c80357b87661c1d
shiun0413 <a0923183408@gmail.com>
2023-05-22 04:22:56 +0800

    refactor(pages/aboutUs): update aboutUs layout

--- src/pages/aboutUs/index.tsx
+++ src/pages/aboutUs/index.tsx
@@ -1,26 +1,22 @@
+import { AboutUsCard } from "@/components/AboutUsCard";
 import Footer from "@/components/Footer";
-import PageTitle from "@/components/PageTitle";
 import { IndexCard } from "@/components/IndexCard";
-import { OrderList } from "@/components/OrderList";
+import PageTitle from "@/components/PageTitle";
 import { BaseLayout } from "@/components/layouts/BaseLayout";
 import { Button } from "@/components/ui/Button";
-import { Link } from "@/components/ui/Link";
 import { Typography } from "@/components/ui/Typography";
 import { useOrders } from "@/hooks/api/useOrders";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { flattenInfinitePaginatedData } from "@/utils/flattenInfinitePaginatedData";
-import { TabContext, TabList, TabPanel } from "@mui/lab";
-import { Tab as MuiTab, Stack, styled } from "@mui/material";
+import { TabContext, TabPanel } from "@mui/lab";
+import { Stack, styled } from "@mui/material";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import Skeleton from "@mui/material/Skeleton";
 import type { InferGetStaticPropsType, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { getPlaiceholder } from "plaiceholder";
 import { useState } from "react";
-import { Height } from "@mui/icons-material";
-import { AboutUsCard } from "@/components/AboutUsCard";
 
 const TABS = [
   { label: "委託者", value: "CONSUMER" },
@@ -56,11 +52,9 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <BaseLayout>
-        <PageTitle title={"關於我們"} />
-
-        {/* <Container sx={{ my: 6 }}> */}
+        <PageTitle title="關於我們" />
         <Stack
-          gap={{ xs: 4, md: 6 }}
+          gap={{ xs: 6, md: 6 }}
           direction={{ md: "row" }}
           justifyContent="center"
           alignItems="center"
@@ -73,69 +67,62 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
               sizes="(max-width: 600px) 50vw, 100vw"
               priority={true}
               style={{
-                boxShadow: "0px 2px 4px rgba(51, 88, 145, 0.5)",
-                borderRadius: "6px",
                 overflow: "hidden",
               }}
             />
           </Box>
-          <Box
-            sx={{
-              background: "#c4c4c4",
-              width: "100%",
-            }}
-          >
-            <Typography
-              variant="h1"
-              color="primary.main"
-              weightPreset="bold"
-              textAlign={{ xs: "center", md: "left" }}
-            >
-              創立理念
-            </Typography>
-          </Box>
+
           <Stack justifyContent="center" height="100%">
-            <Box mt={{ xs: 4, md: 6 }}>
+            <Box
+              sx={{
+                backgroundColor: "#c4c4c4",
+                width: "100%",
+                height: "94px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
               <Typography
-                variant="h5"
-                as="p"
-                color="secondary.dark"
-                textAlign={{ xs: "center", md: "left" }}
+                variant="h1"
+                color="base.800"
+                weightPreset="bold"
+                textAlign={{ xs: "center", md: "center" }}
               >
-                還在為了擔心海外代購需要支付的高額國際運費嗎？
-                出國旅遊行李箱的多餘空間不知道要如何運用嗎？
-                還在為了找尋更低價的代購商而到處比價嗎?
-              </Typography>
-              <Typography
-                variant="h5"
-                as="p"
-                color="secondary.dark"
-                textAlign={{ xs: "center", md: "left" }}
-                sx={{ mt: 3 }}
-              >
-                為了解決在代購上遇到的問題，並且實現共享經濟，因此我們團隊決定建立一個能夠快速媒合，且價格公開透明的代購媒合平台。
+                創立理念
               </Typography>
             </Box>
-            {/* <Stack
-              direction={{ xs: "row", md: "column" }}
-              gap={2}
-              mt={{ xs: 3, md: 6 }}
-            >
-              <StyledButton
-                variant="outlined"
-                LinkComponent={Link}
-                href="/trips/new"
-              >
-                新增旅途
-              </StyledButton>
-              <StyledButton LinkComponent={Link} href="/orders/new">
-                新增委託
-              </StyledButton>
-            </Stack> */}
+            <Container sx={{ my: 6 }}>
+              <Box>
+                <Typography
+                  variant="h5"
+                  as="p"
+                  color="base.800"
+                  textAlign={{ xs: "left", md: "left" }}
+                >
+                  還在為了擔心海外代購需要支付的高額國際運費嗎？
+                  <br />
+                  出國旅遊行李箱的多餘空間不知道要如何運用嗎？
+                  <br />
+                  還在為了找尋更低價的代購商而到處比價嗎?
+                </Typography>
+                <Typography
+                  variant="h5"
+                  as="p"
+                  color="base.800"
+                  textAlign={{ xs: "left", md: "left" }}
+                  sx={{ mt: 3 }}
+                >
+                  為了解決在代購上遇到的問題，並且實現<b>共享經濟</b>
+                  ，因此我們團隊決定建立一個能夠<b>快速媒合</b>，且
+                  <b>價格公開透明</b>的代購媒合平台。
+                </Typography>
+              </Box>
+            </Container>
           </Stack>
         </Stack>
         <Box>
-          <Box
+          {/* <Box
             sx={{
               width: "100%",
               Height: 500,
@@ -143,163 +130,169 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
               border: "solid 1px",
               clipPath: "ellipse(48% 40% at 50% 50%)",
             }}
+          ></Box> */}
+
+          <Typography
+            variant="h1"
+            color="base.800"
+            weightPreset="bold"
+            textAlign={{ xs: "center", md: "left" }}
           >
+            核心價值
+          </Typography>
+          <Container sx={{ my: 6 }}>
+            <Stack spacing={4}>
+              <Box>
+                <Typography
+                  variant="h1"
+                  color="primary.main"
+                  weightPreset="bold"
+                  textAlign={{ xs: "left", md: "left" }}
+                >
+                  快速媒合
+                </Typography>
+                <Typography
+                  variant="h5"
+                  as="p"
+                  color="base.800"
+                  textAlign={{ xs: "left", md: "left" }}
+                  sx={{
+                    marginTop: 2,
+                  }}
+                >
+                  根據您的需求與條件，幫你快速媒合適合的合作對象。
+                </Typography>
+              </Box>
+
+              <Box>
+                <Typography
+                  variant="h1"
+                  color="primary.main"
+                  weightPreset="bold"
+                  textAlign={{ xs: "left", md: "left" }}
+                >
+                  共享經濟
+                </Typography>
+                <Typography
+                  variant="h5"
+                  as="p"
+                  color="base.800"
+                  textAlign={{ xs: "left", md: "left" }}
+                  sx={{
+                    marginTop: 2,
+                  }}
+                >
+                  利用行李箱的閒置空間，實現共享經濟的概念。
+                </Typography>
+              </Box>
+              <Box>
+                <Typography
+                  variant="h1"
+                  color="primary.main"
+                  weightPreset="bold"
+                  textAlign={{ xs: "left", md: "left" }}
+                >
+                  價格公開透明
+                </Typography>
+                <Typography
+                  variant="h5"
+                  as="p"
+                  color="base.800"
+                  textAlign={{ xs: "left", md: "left" }}
+                  sx={{
+                    marginTop: 2,
+                  }}
+                >
+                  所有價格公開透明，幫您省去溝通上的時間成本。
+                </Typography>
+              </Box>
+            </Stack>
+          </Container>
+        </Box>
+        <Container sx={{ my: 6 }}>
+          <Box>
             <Typography
               variant="h1"
-              color="primary.main"
+              color="base.800"
               weightPreset="bold"
               textAlign={{ xs: "center", md: "left" }}
               sx={{
                 marginTop: 10,
-                marginBottom: 5,
                 display: "flex",
                 justifyContent: "center",
+                marginBottom: 3,
               }}
             >
-              核心價值
+              創辦團隊
             </Typography>
           </Box>
 
-          <Stack spacing={1}>
-            <Box>
-              <Typography
-                variant="h1"
-                color="primary.main"
-                weightPreset="bold"
-                textAlign={{ xs: "left", md: "left" }}
-              >
-                快速媒合
-              </Typography>
-              <Typography
-                variant="h5"
-                as="p"
-                color="secondary.dark"
-                textAlign={{ xs: "center", md: "left" }}
-              >
-                根據您的需求與條件，幫你快速媒合適合的合作對象。
-              </Typography>
-            </Box>
-
-            <Box>
-              <Typography
-                variant="h1"
-                color="primary.main"
-                weightPreset="bold"
-                textAlign={{ xs: "left", md: "left" }}
-              >
-                共享經濟
-              </Typography>
-              <Typography
-                variant="h5"
-                as="p"
-                color="secondary.dark"
-                textAlign={{ xs: "center", md: "left" }}
-              >
-                利用行李箱的閒置空間，實現共享經濟的概念。
-              </Typography>
-            </Box>
-            <Box>
-              <Typography
-                variant="h1"
-                color="primary.main"
-                weightPreset="bold"
-                textAlign={{ xs: "left", md: "left" }}
-              >
-                價格公開透明
-              </Typography>
-              <Typography
-                variant="h5"
-                as="p"
-                color="secondary.dark"
-                textAlign={{ xs: "center", md: "left" }}
-              >
-                所有價格公開透明，幫您省去溝通上的時間成本。
-              </Typography>
-            </Box>
-          </Stack>
-        </Box>
-
-        <Typography
-          variant="h1"
-          color="primary.main"
-          weightPreset="bold"
-          textAlign={{ xs: "center", md: "left" }}
-          sx={{ marginTop: 10, display: "flex", justifyContent: "center" }}
-        >
-          創辦團隊
-        </Typography>
-        <TabContext value={currentTab}>
-          <Box
-            sx={{
-              borderBottom: 1,
-              borderColor: "divider",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <TabList
-              centered
-              onChange={(_e, v) => setCurrentTab(v)}
-              aria-label="委託者 / 代購者"
+          <Box>
+            <Typography
+              variant="h5"
+              as="p"
+              color="base.800"
+              textAlign={{ xs: "left", md: "left" }}
             >
-              {TABS.map((tab) => (
-                <MuiTab
-                  key={tab.value}
-                  label={tab.label}
-                  value={tab.value}
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    fontSize: (theme) => ({
-                      xs: theme.typography.pxToRem(16),
-                      sm: theme.typography.pxToRem(18),
-                    }),
-                  }}
-                />
-              ))}
-            </TabList>
+              我們是一群來自輔仁大學資管系的學子， 於 2022 年聚集而成的團隊。
+            </Typography>
+            <Typography
+              variant="h5"
+              as="p"
+              color="base.800"
+              textAlign={{ xs: "left", md: "left" }}
+              sx={{ mt: 3 }}
+            >
+              我們憑藉著專業知識與創新思維，專注於解決社會問題並創造有價值的技術產品。
+            </Typography>
+            <Typography
+              variant="h5"
+              as="p"
+              color="base.800"
+              textAlign={{ xs: "left", md: "left" }}
+              sx={{ mt: 3 }}
+            >
+              我們的理念不只是創造出一個共享經濟、快速媒合、價格透明的，更是透過實際行動，以科技助益社會，
+              期望能共創一個高度透明、充分利用共享經濟並為每位用戶帶來價值的代購市場，讓每位用戶都能從中受益。
+            </Typography>
           </Box>
+        </Container>
 
+        <TabContext value={currentTab}>
           <TabPanel value="CONSUMER">
             <Stack
               direction="column"
               justifyContent="center"
               alignItems="center"
-              spacing={6}
+              spacing={3}
             >
               <AboutUsCard
-                step={1 - 1}
-                title="邱奕勳"
-                content="PM、後端開發"
+                name="邱奕勳"
+                job="PM、後端開發"
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 ImageProps={images.howItWorks.consumer[0]!}
               />
               <AboutUsCard
-                step={1 - 1}
-                title="陳俊廷"
-                content="設計、行銷"
+                name="陳俊廷"
+                job="設計、行銷"
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 ImageProps={images.howItWorks.consumer[0]!}
               />
               <AboutUsCard
-                step={1 - 1}
-                title="范詠淇"
-                content="設計、行銷"
+                name="范詠淇"
+                job="設計、行銷"
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 ImageProps={images.howItWorks.consumer[0]!}
               />
               <AboutUsCard
-                step={1 - 1}
-                title="邱奕勳"
-                content="PM、後端開發"
+                name="邱奕勳"
+                job="PM、後端開發"
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 ImageProps={images.howItWorks.consumer[0]!}
               />
               <AboutUsCard
-                step={1 - 1}
-                title="邱奕勳"
-                content="PM、後端開發"
+                name="邱奕勳"
+                job="PM、後端開發"
                 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                 ImageProps={images.howItWorks.consumer[0]!}
               />
@@ -396,7 +389,7 @@ export const getStaticProps = async () => {
   };
 
   const aboutUs = {
-    aboutUs: await getImagesBase64(["/images/about-us/about-us-cover.jpg"], {
+    aboutUs: await getImagesBase64(["/images/about-us/about-us-cover-2.jpg"], {
       alt: "about us",
     }),
   };
