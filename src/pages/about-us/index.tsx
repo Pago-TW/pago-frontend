aed4f3c44d1c926806aa76f3673bf7a5e0e1bad9
Kerati <enderwolf50@gmail.com>
2023-05-22 11:10:40 +0800

    fix(/about-us): just fix smth

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -44,79 +44,84 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
               }}
             />
           </Box>
-
-          <Stack justifyContent="center" height="100%">
-            <Box
-              sx={{
-                backgroundColor: "#c4c4c4",
-                width: "100%",
-                height: "94px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
+        </Stack>
+        <Stack justifyContent="center" height="100%" sx={{ mt: 6 }}>
+          <Box
+            sx={{
+              backgroundColor: "#c4c4c4",
+              width: "100%",
+              height: "94px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography
+              variant="h1"
+              color="base.800"
+              weightPreset="bold"
+              textAlign={{ xs: "center", md: "center" }}
+              lineHeight={1.75}
             >
+              創立理念
+            </Typography>
+          </Box>
+          <Container sx={{ my: 6 }}>
+            <Box>
               <Typography
-                variant="h1"
+                variant="h5"
+                as="p"
                 color="base.800"
-                weightPreset="bold"
-                textAlign={{ xs: "center", md: "center" }}
+                textAlign={{ xs: "left", md: "left" }}
+                lineHeight={1.75}
               >
-                創立理念
+                還在為了擔心海外代購需要支付的高額國際運費嗎？
+              </Typography>
+              <Typography
+                variant="h5"
+                as="p"
+                color="base.800"
+                textAlign={{ xs: "left", md: "left" }}
+                lineHeight={1.75}
+              >
+                出國旅遊行李箱的多餘空間不知道要如何運用嗎？
+              </Typography>
+              <Typography
+                variant="h5"
+                as="p"
+                color="base.800"
+                textAlign={{ xs: "left", md: "left" }}
+                lineHeight={1.75}
+              >
+                還在為了找尋更低價的代購商而到處比價嗎?
+              </Typography>
+              <Typography
+                variant="h5"
+                as="p"
+                color="base.800"
+                textAlign={{ xs: "left", md: "left" }}
+                lineHeight={1.75}
+                sx={{ mt: 3 }}
+              >
+                為了解決在代購上遇到的問題，並且實現<b>共享經濟</b>
+                ，因此我們團隊決定建立一個能夠<b>快速媒合</b>，且
+                <b>價格公開透明</b>的代購媒合平台。
               </Typography>
             </Box>
-            <Container sx={{ my: 6 }}>
-              <Box>
-                <Typography
-                  variant="h5"
-                  as="p"
-                  color="base.800"
-                  textAlign={{ xs: "left", md: "left" }}
-                >
-                  還在為了擔心海外代購需要支付的高額國際運費嗎？
-                  <br />
-                  出國旅遊行李箱的多餘空間不知道要如何運用嗎？
-                  <br />
-                  還在為了找尋更低價的代購商而到處比價嗎?
-                </Typography>
-                <Typography
-                  variant="h5"
-                  as="p"
-                  color="base.800"
-                  textAlign={{ xs: "left", md: "left" }}
-                  sx={{ mt: 3 }}
-                >
-                  為了解決在代購上遇到的問題，並且實現<b>共享經濟</b>
-                  ，因此我們團隊決定建立一個能夠<b>快速媒合</b>，且
-                  <b>價格公開透明</b>的代購媒合平台。
-                </Typography>
-              </Box>
-            </Container>
-          </Stack>
+          </Container>
         </Stack>
-        <Box>
-          {/* <Box
-            sx={{
-              width: "100%",
-              Height: 500,
-              backgroundColor: "#e1e1e1",
-              border: "solid 1px",
-              clipPath: "ellipse(48% 40% at 50% 50%)",
-            }}
-          ></Box> */}
-
-          {/* TODO HELP_ME: "核心價值"這裡根本大魔王關卡 */}
+        <div>
           <Typography
             variant="h1"
             color="base.800"
             weightPreset="bold"
-            textAlign={{ xs: "center", md: "left" }}
+            textAlign={{ xs: "center", md: "center" }}
           >
             核心價值
           </Typography>
-          <Container sx={{ my: 6 }}>
+          <Container>
             <Stack spacing={4}>
-              <Box>
+              <div>
                 <Typography
                   variant="h1"
                   color="primary.main"
@@ -136,9 +141,8 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
                 >
                   根據您的需求與條件，幫你快速媒合適合的合作對象。
                 </Typography>
-              </Box>
-
-              <Box>
+              </div>
+              <div>
                 <Typography
                   variant="h1"
                   color="primary.main"
@@ -158,8 +162,8 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
                 >
                   利用行李箱的閒置空間，實現共享經濟的概念。
                 </Typography>
-              </Box>
-              <Box>
+              </div>
+              <div>
                 <Typography
                   variant="h1"
                   color="primary.main"
@@ -179,12 +183,12 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
                 >
                   所有價格公開透明，幫您省去溝通上的時間成本。
                 </Typography>
-              </Box>
+              </div>
             </Stack>
           </Container>
-        </Box>
-        <Container sx={{ my: 6 }}>
-          <Box>
+        </div>
+        <Container>
+          <Box sx={{ my: 6 }}>
             <Typography
               variant="h1"
               color="base.800"
@@ -199,36 +203,38 @@ const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
             >
               創辦團隊
             </Typography>
-          </Box>
-
-          <Box>
-            <Typography
-              variant="h5"
-              as="p"
-              color="base.800"
-              textAlign={{ xs: "left", md: "left" }}
-            >
-              我們是一群來自輔仁大學資管系的學子， 於 2022 年聚集而成的團隊。
-            </Typography>
-            <Typography
-              variant="h5"
-              as="p"
-              color="base.800"
-              textAlign={{ xs: "left", md: "left" }}
-              sx={{ mt: 3 }}
-            >
-              我們憑藉著專業知識與創新思維，專注於解決社會問題並創造有價值的技術產品。
-            </Typography>
-            <Typography
-              variant="h5"
-              as="p"
-              color="base.800"
-              textAlign={{ xs: "left", md: "left" }}
-              sx={{ mt: 3 }}
-            >
-              我們的理念不只是創造出一個共享經濟、快速媒合、價格透明的，更是透過實際行動，以科技助益社會，
-              期望能共創一個高度透明、充分利用共享經濟並為每位用戶帶來價值的代購市場，讓每位用戶都能從中受益。
-            </Typography>
+            <Box>
+              <Typography
+                variant="h5"
+                as="p"
+                color="base.800"
+                lineHeight={1.75}
+                textAlign={{ xs: "left", md: "left" }}
+              >
+                我們是一群來自輔仁大學資管系的學子， 於 2022 年聚集而成的團隊。
+              </Typography>
+              <Typography
+                variant="h5"
+                as="p"
+                color="base.800"
+                textAlign={{ xs: "left", md: "left" }}
+                lineHeight={1.75}
+                sx={{ mt: 3 }}
+              >
+                我們憑藉著專業知識與創新思維，專注於解決社會問題並創造有價值的技術產品。
+              </Typography>
+              <Typography
+                variant="h5"
+                as="p"
+                color="base.800"
+                textAlign={{ xs: "left", md: "left" }}
+                lineHeight={1.75}
+                sx={{ mt: 3 }}
+              >
+                我們的理念不只是創造出一個共享經濟、快速媒合、價格透明的，更是透過實際行動，以科技助益社會，
+                期望能共創一個高度透明、充分利用共享經濟並為每位用戶帶來價值的代購市場，讓每位用戶都能從中受益。
+              </Typography>
+            </Box>
           </Box>
           <Stack
             direction="column"
