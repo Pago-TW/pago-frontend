accd8b8bb5ad7b09b5c7eb15ca4525c0882c4be5
Kerati <enderwolf50@gmail.com>
2023-09-01 17:38:17 +0800

    fix(mui): set `useFlexGap=true` on mui's Stack component by default

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -12,6 +12,11 @@ export const theme = createTheme({
         },
       },
     },
+    MuiStack: {
+      defaultProps: {
+        useFlexGap: true,
+      },
+    },
   },
   palette: {
     background: {
