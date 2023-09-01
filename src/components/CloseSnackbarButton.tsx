fe191c58121f936a232dc182367ff4e5c93f1a7b
Kerati <enderwolf50@gmail.com>
2023-09-01 18:16:03 +0800

    style(prettier): just... prettier

--- src/components/CloseSnackbarButton.tsx
+++ src/components/CloseSnackbarButton.tsx
@@ -2,7 +2,9 @@ import { Close } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import { useSnackbar, type SnackbarKey } from "notistack";
 
-export interface CloseSnackbarButtonProps { snackbarKey: SnackbarKey }
+export interface CloseSnackbarButtonProps {
+  snackbarKey: SnackbarKey;
+}
 
 export const CloseSnackbarButton = ({
   snackbarKey,
