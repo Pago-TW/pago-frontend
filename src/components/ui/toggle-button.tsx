079eb346cff8d2f2df0725920c66eadcadbb5b28
Kerati <enderwolf50@gmail.com>
2023-09-17 16:27:28 +0800

    refactor(toggle-button): set border and text color when color="pago"

--- src/components/ui/toggle-button.tsx
+++ src/components/ui/toggle-button.tsx
@@ -1,9 +1,15 @@
 import { ToggleButton as MuiToggleButton, styled } from "@mui/material";
 
-export const ToggleButton = styled(MuiToggleButton)(({ theme, size }) => ({
-  ...(size === "small" && {
-    padding: theme.spacing("6px", "16px"),
-    fontSize: theme.typography.pxToRem(14),
-    lineHeight: 1.5,
-  }),
-}));
+export const ToggleButton = styled(MuiToggleButton)(
+  ({ theme, size, color }) => ({
+    ...(size === "small" && {
+      padding: theme.spacing("6px", "16px"),
+      fontSize: theme.typography.pxToRem(14),
+      lineHeight: 1.5,
+    }),
+    ...(color === "pago" && {
+      borderColor: theme.palette.pago.main,
+      color: theme.palette.pago.main,
+    }),
+  })
+);
