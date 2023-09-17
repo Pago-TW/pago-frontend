0b676bd8956065559a5e87908d59402b92a77230
Kerati <enderwolf50@gmail.com>
2023-09-17 12:32:37 +0800

    refactor(hooks/api): update params type and get page index

--- src/hooks/api/use-trip-collection.ts
+++ src/hooks/api/use-trip-collection.ts
@@ -3,8 +3,14 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { axios } from "@/libs/axios";
 import type { PaginatedResponse, PaginationParams } from "@/types/api";
 import type { TripCollection } from "@/types/trip";
+import { getLastIndex } from "@/utils/api";
 
-type Params = PaginationParams;
+type Params = PaginationParams<
+  {
+    search: string;
+  },
+  TripCollection
+>;
 
 interface Options {
   params?: Params;
@@ -27,9 +33,13 @@ const getTripCollections = async (options: Options = {}) => {
   return res.data;
 };
 
-export const useTripCollections = () => {
+export const useTripCollections = (params?: Params) => {
   return useInfiniteQuery({
-    queryKey: ["trips", "collections"],
-    queryFn: ({ pageParam }) => getTripCollections({ pageParam }),
+    queryKey: ["trips", "collections", params],
+    queryFn: ({ pageParam }) => getTripCollections({ params, pageParam }),
+    getNextPageParam: (lastPage) => {
+      const lastIndex = getLastIndex(lastPage);
+      return lastIndex < lastPage.total ? lastIndex + 1 : undefined;
+    },
   });
 };
