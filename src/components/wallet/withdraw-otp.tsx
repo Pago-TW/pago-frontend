c27cf2a0e87e3c8131c42e82345955c8f08928c5
Kerati <enderwolf50@gmail.com>
2023-09-26 00:17:14 +0800

    feat: countdown after send otp

--- src/components/wallet/withdraw-otp.tsx
+++ src/components/wallet/withdraw-otp.tsx
@@ -1,14 +1,76 @@
+import { useEffect, useRef, useState } from "react";
+
 import { InputLabel, Stack } from "@mui/material";
+import { AxiosError } from "axios";
+import Countdown, { type CountdownRenderProps } from "react-countdown";
 import { useFormContext } from "react-hook-form";
 
 import { OtpInput } from "@/components/inputs/otp-input";
 import { Button } from "@/components/ui/button";
 import { useRequestOtp } from "@/hooks/api/use-request-otp";
+import { calcCountdownDate } from "@/utils/misc";
 
 export const WithdrawOtp = () => {
-  const { control } = useFormContext();
+  const { control, setError } = useFormContext();
+
+  const [countdownDate, setCountdownDate] = useState<Date>(new Date());
+  const countdownRef = useRef<Countdown>(null);
 
   const { mutate: requestOtp } = useRequestOtp();
+
+  useEffect(() => {
+    countdownRef.current?.start();
+  }, [countdownDate]);
+
+  const handleRequestOtp = () => {
+    requestOtp(undefined, {
+      onSuccess: (data) => {
+        const calculatedDate = calcCountdownDate(data.createDate);
+        setCountdownDate(calculatedDate);
+      },
+      onError: (error) => {
+        if (error instanceof AxiosError) {
+          if (error.response?.status === 429) {
+            const {
+              createDate,
+              secondsRemaining,
+            }: { createDate: string; secondsRemaining: number } =
+              error.response.data;
+            if (createDate) {
+              const calculatedDate = calcCountdownDate(
+                createDate,
+                secondsRemaining
+              );
+              setCountdownDate(calculatedDate);
+            } else {
+              setError("otpCode", {
+                message: "請求次數已超過上限，請明天再試",
+              });
+            }
+          }
+        }
+      },
+    });
+  };
+
+  const countdownButtonRenderer = ({
+    completed,
+    formatted: { minutes, seconds },
+  }: CountdownRenderProps) => (
+    <Button
+      variant="outlined"
+      size="small"
+      onClick={handleRequestOtp}
+      disabled={!completed}
+      sx={{
+        minWidth: "fit-content",
+        px: 2,
+        fontSize: (theme) => theme.typography.pxToRem(12),
+      }}
+    >
+      {completed ? "取得驗證碼" : `${minutes}:${seconds}`}
+    </Button>
+  );
 
   return (
     <div>
@@ -24,18 +86,11 @@ export const WithdrawOtp = () => {
             validateChar: (char) => char === "" || /\d/.test(char),
           }}
         />
-        <Button
-          variant="outlined"
-          size="small"
-          onClick={() => requestOtp()}
-          sx={{
-            minWidth: "fit-content",
-            px: 2,
-            fontSize: (theme) => theme.typography.pxToRem(12),
-          }}
-        >
-          取得驗證碼
-        </Button>
+        <Countdown
+          date={countdownDate}
+          ref={countdownRef}
+          renderer={countdownButtonRenderer}
+        />
       </Stack>
     </div>
   );
