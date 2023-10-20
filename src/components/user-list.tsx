d57428d4e8ff2cd042b58ff46f52021525a10527
Kerati <enderwolf50@gmail.com>
2023-10-20 22:48:02 +0800

    refactor: extract user list component without testing
    idk if it works :/

---
+++ src/components/user-list.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from "react";
+
+import { Stack, type StackProps } from "@mui/material";
+
+import type { UserItem } from "@/components/user-item";
+
+export type UserListProps = Omit<StackProps, "children"> & {
+  children: ReactElement<typeof UserItem>[];
+};
+
+export const UserList = (props: UserListProps) => <Stack gap={3} {...props} />;
