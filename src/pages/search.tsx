46ad91863db9f6e1131cb98080b1bef267f61d04
Kerati <enderwolf50@gmail.com>
2023-10-20 23:06:01 +0800

    feat: no search result text & auto fetch next page (prob)

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,16 +1,20 @@
-import { useMemo, type SyntheticEvent } from "react";
+import { useEffect, useMemo, type SyntheticEvent } from "react";
 import type { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-import { Container, styled, Tab, Tabs } from "@mui/material";
+import { Box, Container, styled, Tab, Tabs } from "@mui/material";
+import { useInView } from "react-intersection-observer";
 
 import { BaseLayout } from "@/components/layouts/base-layout";
 import OrderList from "@/components/order-list";
+import { ShowMoreButton } from "@/components/show-more-button";
 import TripList from "@/components/trip-list";
+import { Typography } from "@/components/ui/typography";
 import { UserItem } from "@/components/user-item";
 import { UserList } from "@/components/user-list";
 import { useSearch, type SearchType } from "@/hooks/api/use-search";
+import { useMediaQuery } from "@/hooks/use-media-query";
 import type { Order } from "@/types/order";
 import type { Trip } from "@/types/trip";
 import type { User } from "@/types/user";
@@ -32,12 +36,27 @@ export default function SearchPage() {
   const q = router.query.q as string;
   const type = router.query.type as SearchType;
 
-  const { data: itemsData } = useSearch({ params: { type, query: q } });
+  const mdUp = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
+  const { ref, inView } = useInView();
+
+  const {
+    data: itemsData,
+    isFetching,
+    hasNextPage,
+    fetchNextPage,
+  } = useSearch({
+    params: { type, query: q },
+  });
 
   const items = useMemo(
     () => flattenInfinitePaginatedData<Order | Trip | User>(itemsData),
     [itemsData]
   );
+
+  useEffect(() => {
+    if (inView && hasNextPage) void fetchNextPage();
+  }, [inView, hasNextPage, fetchNextPage]);
 
   const handleTabChange = (_e: SyntheticEvent, value: SearchType) => {
     void router.push(
@@ -54,6 +73,8 @@ export default function SearchPage() {
     );
   };
 
+  const handleShowMore = () => fetchNextPage();
+
   return (
     <>
       <Head>
@@ -67,15 +88,38 @@ export default function SearchPage() {
             <StyledTab value="trip" label="旅途" />
             <StyledTab value="user" label="使用者" />
           </Tabs>
-          {type === "order" && <OrderList items={items as Order[]} />}
-          {type === "trip" && <TripList items={items as Trip[]} />}
-          {type === "user" && (
-            <UserList gap={4}>
-              {(items as User[]).map((item) => (
-                <UserItem key={item.userId} {...item} />
-              ))}
-            </UserList>
-          )}
+          <Box mt={{ xs: 3, md: 6 }}>
+            {items.length !== 0 ? (
+              <>
+                {type === "order" && <OrderList items={items as Order[]} />}
+                {type === "trip" && <TripList items={items as Trip[]} />}
+                {type === "user" && (
+                  <UserList gap={4}>
+                    {(items as User[]).map((item) => (
+                      <UserItem key={item.userId} {...item} />
+                    ))}
+                  </UserList>
+                )}
+              </>
+            ) : null}
+            {!isFetching && items.length === 0 ? (
+              <Typography
+                variant={mdUp ? "h3" : "h5"}
+                component="p"
+                textAlign="center"
+              >
+                查無符合「{q}」的結果
+              </Typography>
+            ) : null}
+            <ShowMoreButton
+              hasMore={hasNextPage}
+              onClick={handleShowMore}
+              loading={isFetching}
+              fullWidth
+              ref={ref}
+              sx={{ mt: 2 }}
+            />
+          </Box>
         </Container>
       </BaseLayout>
     </>
